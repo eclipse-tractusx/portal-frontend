d212846d329bd1441c163467c1abeddc587bf585
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-04-01 11:25:42 +0200

    renamed file with case issue

--- cx-portal/src/components/pages/Dashboard/index.tsx
+++ cx-portal/src/components/pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next'
 import { Typography } from 'cx-portal-shared-components'
 import SearchSection from './components/SearchSection'
 import StageSection from './components/StageSection'
-import './dashboard.scss'
+import './DashboardStyle.scss'
 
 export default function Dashboard() {
   const { t } = useTranslation()
