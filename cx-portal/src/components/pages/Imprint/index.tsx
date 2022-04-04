79223e26128477358b88e4c19729d203f93a17c2
Nidhi garg <nidhi.garg@partner.bmw.in>
2022-04-04 18:08:45 +0530

    add imprint page locals

--- cx-portal/src/components/pages/Imprint/index.tsx
+++ cx-portal/src/components/pages/Imprint/index.tsx
@@ -1,3 +1,4 @@
+import { Typography } from 'cx-portal-shared-components'
 import { useTranslation } from 'react-i18next'
 
 export default function Imprint() {
@@ -5,7 +6,23 @@ export default function Imprint() {
   return (
     <main>
       <h2>{t('pages.imprint')}</h2>
-      <p>{t('content.imprint.message')}</p>
+      <Typography variant="h5">{t('content.imprint.directors')}</Typography>
+      <Typography variant="body2">Oliver Ganser ({t('content.imprint.ceo')})</Typography>
+      <Typography variant="body2">Prof. Dr.-Ing. Boris Otto ({t('content.imprint.deputyCeo')})</Typography>
+      <Typography variant="body2">Claus Cremers ({t('content.imprint.treasurer')})</Typography>
+      <br />
+      <Typography variant="body2">{t('content.imprint.address')}</Typography>
+      <Typography variant="body2">Catena-X Automotive Network e.V.</Typography>
+      <Typography variant="body2">c/o IFOK GmbH</Typography>
+      <Typography variant="body2">Reinhardtstraße 58</Typography>
+      <Typography variant="body2">10117 Berlin</Typography>
+      <br />
+      <Typography variant="h5">{t('content.imprint.contact&support')}</Typography>
+      <Typography variant="body2">{t('content.imprint.contact&supportDesc')}</Typography>
+      <br />
+      <Typography variant="h5">{t('content.imprint.privacy')}</Typography>
+      <Typography variant="body2">{t('content.imprint.privacyDesc')}</Typography>
+      <a>xxx.xxx.xxx</a>
     </main>
   )
 }
