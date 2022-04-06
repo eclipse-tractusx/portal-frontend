522c481372d64dcab1661837af8e0f4ab64c5b05
floriansommerraysono <florian.sommer@raysonso.com>
2022-04-06 11:50:28 +0200

    CPLP-455/-456: Integration of app lists and category view

--- cx-portal/src/components/pages/AppMarketplace/index.tsx
+++ cx-portal/src/components/pages/AppMarketplace/index.tsx
@@ -1,14 +1,94 @@
+import { GroupItemView } from 'components/shared/basic/GroupItemView'
+import { SearchInput, Typography } from 'cx-portal-shared-components'
+import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchApps } from 'state/features/appMarketplace/appMarketplaceActions'
+import { selectorAppMarketplace } from 'state/features/appMarketplace/appMarketplaceSlice'
+import { selectorUser } from 'state/features/user/userSlice'
 import StageSection from './components/StageSection'
+import { FilterSelector } from 'cx-portal-shared-components'
 import './AppMarketplace.scss'
-import FavoriteSection from './components/FavoriteSection'
-import AppListSection from './components/AppListSection'
+import { Box } from '@mui/material'
 
 export default function AppMarketplace() {
+  const dispatch = useDispatch()
+  const { apps } = useSelector(selectorAppMarketplace)
+  const { t } = useTranslation()
+  const { token } = useSelector(selectorUser)
+
+  const [group, setGroup] = useState<string>('')
+
+  const setView = (e: React.MouseEvent<HTMLInputElement>) => {
+    setGroup(e.currentTarget.value)
+  }
+
+  const viewCategories = [
+    {
+      buttonText: t('content.appstore.appOverviewSection.viewCategories.all'),
+      buttonValue: '',
+      onButtonClick: setView,
+    },
+    {
+      buttonText: t(
+        'content.appstore.appOverviewSection.viewCategories.useCases'
+      ),
+      buttonValue: 'useCases',
+      onButtonClick: setView,
+    },
+  ]
+
+  useEffect(() => {
+    if (token) {
+      dispatch(fetchApps())
+    }
+  }, [token, dispatch])
+
   return (
     <main className="app-store">
       <StageSection />
-      <FavoriteSection />
-      <AppListSection />
+      <section className="overview-section">
+        <Typography variant="h3" className="section-title">
+          {t('content.appstore.appOverviewSection.title')}
+        </Typography>
+
+        <FilterSelector activeFilter={group} filterViews={viewCategories} />
+        <Box sx={{ textAlign: 'center' }}>
+          <SearchInput
+            sx={{ minWidth: '544px' }}
+            margin={'normal'}
+            placeholder={t('content.dashboard.searchSection.inputPlaceholder')}
+          />
+        </Box>
+
+        {/*<div className="GroupSelect">*/}
+        {/*  <input*/}
+        {/*    type="radio"*/}
+        {/*    name="group"*/}
+        {/*    value=""*/}
+        {/*    checked={group === ''}*/}
+        {/*    onChange={setView}*/}
+        {/*  />*/}
+        {/*  none*/}
+        {/*  <input*/}
+        {/*    type="radio"*/}
+        {/*    name="group"*/}
+        {/*    value="useCases"*/}
+        {/*    checked={group === 'useCases'}*/}
+        {/*    onChange={setView}*/}
+        {/*  />*/}
+        {/*  use cases*/}
+        {/*  <input*/}
+        {/*    type="radio"*/}
+        {/*    name="group"*/}
+        {/*    value="subtitle"*/}
+        {/*    checked={group === 'subtitle'}*/}
+        {/*    onChange={setView}*/}
+        {/*  />*/}
+        {/*  vendor*/}
+        {/*</div>*/}
+        <GroupItemView items={apps} groupKey={group} />
+      </section>
     </main>
   )
 }
--- cx-portal/src/components/shared/basic/GroupItemView/index.tsx
+++ cx-portal/src/components/shared/basic/GroupItemView/index.tsx
@@ -1,6 +1,8 @@
 import { multiMapBy } from 'utils/multiMapBy'
 import { RawItemView } from '../RawItemView'
 import './GroupItemView.scss'
+import { CategoryDivider } from 'cx-portal-shared-components'
+import { useTranslation } from 'react-i18next'
 
 export const GroupItemView = ({
   items,
@@ -9,6 +11,8 @@ export const GroupItemView = ({
   items: any[]
   groupKey: string
 }) => {
+  const { t } = useTranslation()
+
   if (!groupKey || groupKey === '') {
     return <RawItemView items={items} />
   }
@@ -16,15 +20,26 @@ export const GroupItemView = ({
   const group = multiMapBy(items, (item) => item[groupKey])
 
   return (
-    <ul className="GroupItemView">
-      {Object.entries(group).map((v) => (
-        <li key={v[0]}>
-          <p>
-            <span>{v[0]}</span>
-          </p>
-          <RawItemView items={v[1]} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <CategoryDivider
+        buttonText={t('global.actions.more')}
+        categoryItemsLength={128}
+        categoryName="Favorites"
+        onButtonClick={() => {
+          console.log('Category divider')
+        }}
+      />
+      <div>Apps by use case</div>
+      <ul className="GroupItemView">
+        {Object.entries(group).map((v) => (
+          <li key={v[0]}>
+            <p>
+              <span>{v[0]}</span>
+            </p>
+            <RawItemView items={v[1]} />
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
