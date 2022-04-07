9bbab8c5c6bc2cefdf42ff5f696b96ec554a2bb8
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-04-07 11:07:47 +0200

    removed unused import

--- cx-portal-shared-components/src/components/basic/Table/components/Toolbar/helper.test.ts
+++ cx-portal-shared-components/src/components/basic/Table/components/Toolbar/helper.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, test } from '@jest/globals'
+import { describe, expect, it } from '@jest/globals'
 import { SelectedFilter, ToolbarProps } from '.'
 import { initSelectedFilter, getSelectedFilterUpdate } from './helper'
 
