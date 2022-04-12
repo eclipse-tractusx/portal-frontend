9d72be8b7a1e3f4d8c77d034947b0a274e34b00a
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-04-12 09:53:18 +0200

    fixed list key issue

--- cx-portal/src/components/pages/CookiePolicy/index.tsx
+++ cx-portal/src/components/pages/CookiePolicy/index.tsx
@@ -12,14 +12,14 @@ export default function CookiePolicy() {
         <Typography variant="body2">{t('purposes.why')}</Typography>
         <ul>
           {[0, 1, 2, 3, 4].map((n) => (
-            <li>{t(`purposes.reason.${n}`)}</li>
+            <li key={n}>{t(`purposes.reason.${n}`)}</li>
           ))}
         </ul>
         <br />
         <Typography variant="body2">{t('types.message')}</Typography>
         <ul>
           {['strict', 'function', 'target'].map((type) => (
-            <li>
+            <li key={type}>
               {t(`types.${type}`)}
               <table className="cookie-table">
                 <thead>
