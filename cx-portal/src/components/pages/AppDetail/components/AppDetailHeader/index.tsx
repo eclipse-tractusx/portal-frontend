3f71391ef6cf7e12d8c2187d0874604794894855
Nidhi garg <nidhi.garg@partner.bmw.in>
2022-04-20 11:28:30 +0530

    add translationsa and make content hardcoded

--- cx-portal/src/components/pages/AppDetail/components/AppDetailHeader/index.tsx
+++ cx-portal/src/components/pages/AppDetail/components/AppDetailHeader/index.tsx
@@ -2,23 +2,20 @@ import { Button, Rating } from 'cx-portal-shared-components'
 import { useTranslation } from 'react-i18next'
 import './AppDetailHeader.scss'
 
-export interface AppDetailProps {
-  item: any
-}
-
-export default function AppDetailHeader({ item }: AppDetailProps) {
+export default function AppDetailHeader() {
   const { t } = useTranslation()
 
   return (
     <div className="appdetail-header">
-      <img src={item.image.src} />
+      <img src="https://catenaxdev003util.blob.core.windows.net/assets/apps/images/Lead-Dismantler.png" />
+      {/* The below content needs to get replaced as soon as API is available. */}
       <div className="content">
         <p className="provider">Catena-X</p>
-        <h2 className="heading">{item.title}</h2>
-        <Rating defaultRating={item.rating} />
-        <p className="provider">{item.price}</p>
+        <h2 className="heading">Digital Twin Debugger</h2>
+        <Rating defaultRating={4} />
+        <p className="provider">free for use</p>
         <p className="usecase">
-          <b>{t('content.appdetail.usecase')}:</b> {item.useCases}
+          <b>{t('content.appdetail.usecase')}:</b> Digital Debuggers
         </p>
         <p className="language">
           <b>{t('content.appdetail.language')}:</b> DE, EN
--- cx-portal/src/components/pages/AppDetail/index.tsx
+++ cx-portal/src/components/pages/AppDetail/index.tsx
@@ -5,24 +5,10 @@ import AppDetailHeader from './components/AppDetailHeader'
 export default function AppDetail() {
   const { t } = useTranslation()
 
-  const appItem = {
-    id: 'c7b217c5-1002-46bc-bcd2-57df0e9e46d6',
-    title: 'Dismantler App',
-    subtitle: 'SAP',
-    description: 'SAP App Dismantler App Details',
-    price: 'free of charge',
-    rating: 4.1,
-    useCases: ['Circular Economy'],
-    image: {
-      src: 'https://catenaxdev003util.blob.core.windows.net/assets/apps/images/Lead-Dismantler.png',
-      alt: 'Part Chain',
-    },
-  }
-
   return (
     <main>
       <Typography variant="h3">{t('pages.appdetails')}</Typography>
-      <AppDetailHeader item={appItem} />
+      <AppDetailHeader />
     </main>
   )
 }
