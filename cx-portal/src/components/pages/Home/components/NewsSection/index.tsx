0dc502fb4af98588f968483e2f11bfe7aca38336
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-04-21 17:11:45 +0200

    removed token from data loading component

--- cx-portal/src/components/pages/Home/components/NewsSection/index.tsx
+++ cx-portal/src/components/pages/Home/components/NewsSection/index.tsx
@@ -1,21 +1,17 @@
 import { Cards } from 'cx-portal-shared-components'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { selectorUser } from 'state/features/user/userSlice'
 import { fetchItems } from 'state/features/news/actions'
-import './news-section.scss'
 import { newsSelector } from 'state/features/news/slice'
+import './news-section.scss'
 
 export default function NewsSection() {
   const dispatch = useDispatch()
-  const { token } = useSelector(selectorUser)
   const { items } = useSelector(newsSelector)
 
   useEffect(() => {
-    if (token) {
-      dispatch(fetchItems())
-    }
-  }, [token, dispatch])
+    dispatch(fetchItems())
+  }, [dispatch])
 
   return (
     <section className="news-section">
