1af1d441d864809041650bcd71eb2b8f85f128a3
Diana Renz <diana-leontyeva@mail.ru>
2022-04-21 17:07:36 +0200

    add onClick function for dropzone

--- cx-portal-shared-components/src/components/basic/Dropzone/components/Input.tsx
+++ cx-portal-shared-components/src/components/basic/Dropzone/components/Input.tsx
@@ -21,43 +21,54 @@ class Input extends React.PureComponent<IInputProps> {
     return (
       <Box
         sx={{
+          position: 'relative',
           backgroundColor: 'background.background01',
           borderRadius: '40px',
           border: 'none',
           backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='40' ry='40' stroke='%23B6B6B6FF' stroke-width='2' stroke-dasharray='16' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`,
-          padding: '48px 0',
           textAlign: 'center',
-          minHeight: 33,
+          minHeight: 274,
+          label: {
+            padding: '48px 0',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            cursor: 'pointer',
+          },
         }}
       >
-        <CloudUploadIcon fillColor={'#939393'} size={64} />
-        <Typography
-          variant="h4"
-          sx={{
-            display: 'block',
-            fontFamily: 'LibreFranklin-Light',
-            marginTop: '24px',
-          }}
-        >
-          {inputContentTitle}
-        </Typography>
-        <Typography variant="body1" sx={{ display: 'block' }}>
-          {inputContentSubTitle}
-        </Typography>
-        <input
-          className={inputClassName}
-          style={style}
-          type="file"
-          accept={accept}
-          multiple={multiple}
-          disabled={disabled}
-          onChange={async (e) => {
-            const target = e.target
-            const chosenFiles = await getFilesFromEvent(e)
-            onFiles(chosenFiles)
-            target.value = ''
-          }}
-        />
+        <label>
+          <CloudUploadIcon fillColor={'#939393'} size={64} />
+          <Typography
+            variant="h4"
+            sx={{
+              display: 'block',
+              fontFamily: 'LibreFranklin-Light',
+              marginTop: '24px',
+            }}
+          >
+            {inputContentTitle}
+          </Typography>
+          <Typography variant="body1" sx={{ display: 'block' }}>
+            {inputContentSubTitle}
+          </Typography>
+          <input
+            className={inputClassName}
+            style={style}
+            type="file"
+            accept={accept}
+            multiple={multiple}
+            disabled={disabled}
+            onChange={async (e) => {
+              const target = e.target
+              const chosenFiles = await getFilesFromEvent(e)
+              onFiles(chosenFiles)
+              target.value = ''
+            }}
+          />
+        </label>
       </Box>
     )
   }
