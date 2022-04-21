69da514bcaab09b1efd53f658eb38c4ba5002a82
Nidhi garg <nidhi.garg@partner.bmw.in>
2022-04-21 09:18:32 +0530

    add image gallery section

---
+++ cx-portal/src/components/pages/AppDetail/components/AppDetailImageGallery/index.tsx
@@ -0,0 +1,23 @@
+import { useTranslation } from 'react-i18next'
+import './AppDetailImageGallery.scss'
+
+export default function AppDetailHeader() {
+  const { t } = useTranslation()
+
+  return (
+    <div className="appdetail-gallery">
+        <div className="img-col">
+            <img src="https://cdn.pixabay.com/photo/2017/09/05/10/20/business-2717066_1280.jpg" alt="" />
+            <p>Lorem Image Caption</p>
+        </div>
+        <div className="img-col">
+            <img src="https://cdn.pixabay.com/photo/2017/09/05/10/20/business-2717066_1280.jpg" alt="" />
+            <p>Lorem Image Caption</p>
+        </div>
+        <div className="img-col">
+            <img src="https://cdn.pixabay.com/photo/2017/09/05/10/20/business-2717066_1280.jpg" alt="" />
+            <p>Lorem Image Caption</p>
+        </div>
+    </div>
+  )
+}
