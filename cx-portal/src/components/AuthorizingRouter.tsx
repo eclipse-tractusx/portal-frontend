2311e2ca0283ac5039207b869ae46c89c3f0425d
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-04-24 17:09:59 +0200

    testing routes

--- cx-portal/src/components/AuthorizingRouter.tsx
+++ cx-portal/src/components/AuthorizingRouter.tsx
@@ -1,17 +1,18 @@
 import Main from 'components/Main'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import NotFound from 'components/pages/NotFound'
 import AccessService from 'services/AccessService'
 import ScrollToTop from '../utils/ScrollToTop'
 
 const AuthorizingRouter = () => {
+  const routes = AccessService.permittedRoutes()
+  console.log(routes)
+  routes.map(route => console.log(route))
   return (
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Main />}>
           {AccessService.permittedRoutes()}
-          <Route path="*" element={NotFound()} />
         </Route>
       </Routes>
     </BrowserRouter>
