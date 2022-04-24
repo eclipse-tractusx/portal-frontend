678fb45b6812c159636002ad9d1e5987170cc2dc
Feras Ahmad <feras.ahmad@raysono.com>
2022-04-24 13:30:27 +0200

    CPLP-448 remove carousel title, fix styles

--- cx-portal-shared-components/src/components/basic/Carousel/Carousel.stories.tsx
+++ cx-portal-shared-components/src/components/basic/Carousel/Carousel.stories.tsx
@@ -16,32 +16,34 @@ const itemsArray = ['Element 1', 'Element 2', 'Element 3', 'Element 4', 'Element
 const Template: ComponentStory<typeof Component> = (args: any) => (
   <Component {...args}>
     { itemsArray.map((item: string) => {
-        return (
-          <div
-            style={{
-              padding: '28px',
-              borderRadius: '20px',
-              height: '220px',
-              width: '200px',
-              border: '1px',
-              borderStyle: 'solid',
-              borderColor: theme.palette.border.border01,
-            }}
-            key={uniqueId('carousel-item')}>
-            {item}
-          </div>
-        )
-      })
-    }
+      return (
+        <div
+          style={{
+            padding: '28px',
+            borderRadius: '20px',
+            height: '80%',
+            width: 'auto',
+            border: '1px',
+            borderStyle: 'solid',
+            borderColor: theme.palette.border.border01,
+          }}
+          key={uniqueId('carousel-item')}>
+          {item}
+        </div>
+      )
+    })
+    } 
   </Component>
 )
 
 export const Carousel = Template.bind({})
 Carousel.args = {
-  title: 'Carousel Title',
   dots: true,
   infinite: true,
   slidesToShow: 3,
-  itemWidth: 256,
-  itemHeight: 240,
+  itemWidth: 266,
+  itemHeight: 279,
+  gapBetweenSlides: 32,
+  gapToDots: 40,
+  gapToArrows: 32,
 }
--- cx-portal-shared-components/src/components/basic/Carousel/index.tsx
+++ cx-portal-shared-components/src/components/basic/Carousel/index.tsx
@@ -1,23 +1,23 @@
+import { useState, useEffect , Children } from 'react'
 import Slider from 'react-slick'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import { IconButton } from '../IconButton'
 import { Box } from '@mui/material'
-import { Typography } from '../Typography'
-import { useState, Children } from 'react'
 import { theme } from '../../../theme'
 import uniqueId from 'lodash/uniqueId'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-
 export interface CarouselProps {
   children?: React.ReactNode,
-  title?: string,
   dots?: boolean,
   infinite?: boolean,
   slidesToShow?: number, 
   itemWidth?: number,
   itemHeight?: number,
+  gapBetweenSlides?: number,
+  gapToDots?: number,
+  gapToArrows?: number,
 }
 
 function NavArrows(props: any) {
@@ -48,19 +48,35 @@ function NavArrows(props: any) {
 
 export const Carousel = ({
   children,
-  title,
-  dots,
-  infinite,
-  slidesToShow,
-  itemWidth,
-  itemHeight,
+  dots = true,
+  infinite = true,
+  slidesToShow = 4,
+  itemWidth = 266,
+  itemHeight = 279,
+  gapBetweenSlides = 32,
+  gapToDots = 40,
+  gapToArrows = 32,
 }: CarouselProps) => {
   const [showArrows, setShowArrows] = useState(false)
   const onMouseEnter = () => setShowArrows(true)
   const onMouseLeave = () => setShowArrows(false)
-  const arrayChildren = Children.toArray(children);
+  const arrayChildren = Children.toArray(children)
 
-  const settings = {
+  slidesToShow = slidesToShow && slidesToShow > 0 ? slidesToShow : 1
+  slidesToShow = slidesToShow && slidesToShow > arrayChildren.length ? arrayChildren.length : slidesToShow
+  gapToArrows = gapToArrows && gapToArrows > 0 ? gapToArrows : 1
+  gapBetweenSlides = gapBetweenSlides && gapBetweenSlides > 0 ? gapBetweenSlides : 1
+
+  const carouselLeft = gapBetweenSlides ? `-${2 * gapBetweenSlides}px` : 0
+  const slidsInnerGap = gapBetweenSlides ? `${gapBetweenSlides / 2}px` : 0
+  const outerGapToArrow = gapToArrows ? `${gapToArrows}px` : 0;
+  const innerGapToArrow = gapToArrows ? `${gapToArrows / 2}px` : 0;
+  const outerGapToDots = gapToDots && dots ? `${gapToDots}px` : 'auto'
+  const carouselWidth = slidesToShow && itemWidth && gapBetweenSlides && gapToArrows ? 
+    slidesToShow * itemWidth + (slidesToShow * gapBetweenSlides) + 3 * gapToArrows : 
+    'auto'
+
+    const settings = {
     dots: dots,
     infinite: infinite,
     slidesToShow: slidesToShow,
@@ -94,89 +110,83 @@ export const Carousel = ({
   };
 
   return (
-    <div>
-      { title && 
-        <Box>
-          <Typography
-            sx={{
-              marginLeft: 'auto',
-              marginRight: 'auto',
-              marginBottom: '20px',
-              width: 'max-content',
-              fontFamily: 'LibreFranklin-Light'
-            }}
-            variant="h3">
-            {title}
-          </Typography>    
-
-          <Box
-            sx={{
-              width: '60px',
-              marginLeft: 'auto',
-              marginRight: 'auto',
-              border: `2px solid ${theme.palette.text.secondary}`
-            }}/>
-        </Box>
-      }
-
-      <Box
-        sx={{ 
-          li: {
-            margin: 0,
+    <Box
+      sx={{
+        width: carouselWidth,
+        position: 'relative',
+        left: carouselLeft,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        li: {
+          margin: 0,
+          button: {
+            ':before': {
+              opacity: '.9',
+              fontSize: 8,
+            }
+          },
+          '&.slick-active': {
             button: {
               ':before': {
                 opacity: '.9',
-                fontSize: 8,
+                fontSize: 12,
               }
             },
-            '&.slick-active': {
-              button: {
-                ':before': {
-                  opacity: '.9',
-                  fontSize: 12,
-                }
-              },
-            }
-          },
-          '.slick-prev': {
-            ':before': {
-              content: 'none',
-            },
-          },
-          '.slick-next': {
-            ':before': {
-              content: 'none',
-            },
-          },
-          'img': {
-            display: 'initial',
           }
-        }}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
-        <Slider {...settings}>
-          {Children.map(arrayChildren, (child) => {
-            return (
-              <div key={uniqueId('cax-carousel')}>
+        },
+        '.slick-prev': {
+          left: '-33px',
+          ':before': {
+            content: 'none',
+          },
+        },
+        '.slick-next': {
+          ':before': {
+            content: 'none',
+          },
+        },
+        '.slick-slider': {
+          paddingLeft: innerGapToArrow,
+          paddingRight: innerGapToArrow,
+          marginLeft: outerGapToArrow,
+          marginRight: outerGapToArrow,
+        },
+        '.slick-slide': {
+          width: 'max-content !important',
+        },
+        '.slick-dots': {
+          marginLeft: '-20px'
+        },
+        'img': {
+          display: 'initial',
+        }
+      }}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <Slider {...settings}>
+        {Children.map(arrayChildren, (child) => {
+          return (
+            <div key={uniqueId('cax-carousel')}>
+              <Box sx={{display:"flex"}}>
+                <Box sx={{width: slidsInnerGap}}></Box>
                 <Box
                   sx={{
                     width: `${itemWidth}px`,
                     height: `${itemHeight}px`,
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    marginTop: '50px',
-                    marginBottom: '50px',
+                    marginTop: outerGapToDots,
+                    marginBottom: outerGapToDots,
                   }}
                 >
                   {child}
                 </Box>
-              </div>
-            )
-          })
-          }
-        </Slider>
-      </Box>
-    </div>
+                <Box sx={{width: slidsInnerGap}}></Box>
+              </Box>
+            </div>
+          )
+        })
+        }
+      </Slider>
+    </Box>
   )
 }
