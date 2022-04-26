1b4e919a8df3b88ffa6a6cbd5dfc085e5bb0af37
Feras Ahmad <feras.ahmad@raysono.com>
2022-04-26 11:10:46 +0200

    CPLP-451 Carousel for favorite app added

--- cx-portal/src/components/pages/AppMarketplace/components/FavoriteSection/index.tsx
+++ cx-portal/src/components/pages/AppMarketplace/components/FavoriteSection/index.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next'
-import { Cards, Typography } from 'cx-portal-shared-components'
+import { Typography, Carousel, Card } from 'cx-portal-shared-components'
+import uniqueId from 'lodash/uniqueId'
 
 export default function FavoriteSection() {
   const { t } = useTranslation()
@@ -53,28 +54,48 @@ export default function FavoriteSection() {
       description: 'Lorem Ipsum is simply dummy text of the printing.',
       onButtonClick: () => {},
     },
+    {
+      title: 'CE Marketplace',
+      subtitle: 'SAP',
+      image: {
+        src: 'https://laszeray.com/wp-content/uploads/2019/08/laszeray-2-740x450.jpg',
+        alt: 'Catena-X AppCard',
+      },
+      rating: 4.5,
+      price: 'free to use',
+      description: 'Lorem Ipsum is simply dummy text of the printing.',
+      onButtonClick: () => {},
+    },
   ]
 
   return (
     <section className="business-applications-section">
       <Typography
-        sx={{ fontFamily: 'LibreFranklin-Light' }}
+        sx={{ fontFamily: 'LibreFranklin-Light', marginBottom: '48px !important' }}
         variant="h3"
         className="section-title"
       >
         {t('content.appstore.favoriteSection.title')}
       </Typography>
 
-      <Cards
-        items={items} // TODO: Replace from api
-        columns={4}
-        buttonText="Details"
-        imageSize="small"
-        imageShape="round"
-        variant="minimal"
-        expandOnHover={false}
-        filledBackground={true}
-      />
+      <Carousel gapToDots={5}>
+        {items.map((item) => {
+          return (
+            <Card
+              key={uniqueId(item.title)}
+              title={item.title}
+              subtitle={item.subtitle}
+              image={item.image}
+              buttonText="Details"
+              imageSize="small"
+              imageShape="round"
+              variant="minimal"
+              expandOnHover={false}
+              filledBackground={true}
+            />
+          )
+        })}
+      </Carousel>
     </section>
   )
 }
