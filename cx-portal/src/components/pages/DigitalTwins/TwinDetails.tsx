6f10c039e81d8a388559388290015fc850bfcb88
Janine Semmler <janine.semmler@t-systems.com>
2022-05-04 18:08:42 +0200

    add language texts

--- cx-portal/src/components/pages/DigitalTwins/TwinDetails.tsx
+++ cx-portal/src/components/pages/DigitalTwins/TwinDetails.tsx
@@ -3,16 +3,22 @@ import { useState } from "react";
 import { ShellDescriptor } from "state/features/digitalTwins/types";
 import { TwinDetailGrid } from "./TwinDetailGrid";
 import { Grid, Box, Divider, useTheme }  from '@mui/material';
+import { useTranslation } from "react-i18next";
 
 export const TwinDetails = ({ twin }: {twin: ShellDescriptor}) => {
   const [expanded, setExpanded] = useState<string | false>('panel1');
   const theme = useTheme();
+  const { t } = useTranslation();;
 
   const handleChange =
     (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
       setExpanded(newExpanded ? panel : false);
   };
-  const getDesciption = (elem: ShellDescriptor | any) => <Typography sx={{mb: 3, typography: 'body3'}}>{elem.description[0] ? elem.description[0].text : 'This asset has no description'}</Typography>
+  const getDesciption = (elem: ShellDescriptor | any) => 
+    <Typography sx={{mb: 3, typography: 'body3'}}>
+      {elem.description[0] ? elem.description[0].text : t('content.digitaltwin.detail.no_description')}
+    </Typography>
+
   const hasSubmodels = () => twin.submodelDescriptors.length > 0
 
   return(
@@ -29,11 +35,11 @@ export const TwinDetails = ({ twin }: {twin: ShellDescriptor}) => {
             <>
               <Divider sx={{mb: 2, mr: -2, ml: -2}} />
               <Typography sx={{mb: 3, typography: 'label2'}}>
-                Specific Asset IDs
+               {t('content.digitaltwin.detail.assetId')}
               </Typography>
               {twin.submodelDescriptors.length > 0 &&
                 <TwinDetailGrid
-                  topic="Submodel Endpoints:"
+                  topic={t('content.digitaltwin.detail.submodel_endpoints')}
                   content={twin.submodelDescriptors.length} 
                 />
               }
@@ -41,19 +47,19 @@ export const TwinDetails = ({ twin }: {twin: ShellDescriptor}) => {
               {twin.specificAssetIds.map((saId, index) =>
                 <Box key={saId.key}>
                   <TwinDetailGrid
-                    topic="Key:"
+                    topic={t('content.digitaltwin.detail.key')}
                     content={saId.key} 
                   />
                   <Divider sx={{mb: 2, mr: -2, ml: -2}} />
                   <TwinDetailGrid
-                    topic="Value:"
+                    topic={t('content.digitaltwin.detail.value')}
                     content={saId.value} 
                   />
                   {saId.semanticId &&
                     <>
                       <Divider sx={{mb: 2, mr: -2, ml: -2}} />
                       <TwinDetailGrid
-                        topic="Semantic ID:"
+                        topic={t('content.digitaltwin.detail.semanticid')}
                         content={saId.semanticId.value.join(', ')} 
                       />
                     </>
@@ -80,7 +86,7 @@ export const TwinDetails = ({ twin }: {twin: ShellDescriptor}) => {
                 {getDesciption(subModel)}
                 <Divider sx={{mb: 2, mr: -2, ml: -2}} />
                 <TwinDetailGrid
-                  topic="Semantic ID:"
+                  topic={t('content.digitaltwin.detail.semanticid')}
                   link={{
                     pathname: `/semantichub/${encodeURIComponent(subModel.semanticId.value[0])}`,
                     state: subModel.semanticId.value[0]
@@ -90,23 +96,23 @@ export const TwinDetails = ({ twin }: {twin: ShellDescriptor}) => {
                 <Divider sx={{mr: -2, ml: -2}} />
                 <Grid container
                   sx={{width: `calc(100% + ${theme.spacing(4)})`, m: `0 -${theme.spacing(2)}`, p: 2, typography: 'label3', bgcolor: 'background.background09'}}>
-                  <Grid item xs={12}>Endpoints</Grid>
+                  <Grid item xs={12}>{t('content.digitaltwin.detail.endpoints')}</Grid>
                 </Grid>
                 {subModel.endpoints.map((endpoint, index) =>
                   <>
                     <Divider sx={{mb: 2, mr: -2, ml: -2}} />
                     <TwinDetailGrid
-                      topic="Interface:"
+                      topic={t('content.digitaltwin.detail.interface')}
                       content={endpoint.interface} 
                     />
                     <Divider sx={{mb: 2, mr: -2, ml: -2}} />
                     <TwinDetailGrid
-                      topic="Protocol:"
+                      topic={t('content.digitaltwin.detail.protocol')}
                       content={endpoint.protocolInformation.endpointProtocol} 
                     />
                     <Divider sx={{mb: 2, mr: -2, ml: -2}} />
                     <TwinDetailGrid
-                      topic="Protocol Version:"
+                      topic={t('content.digitaltwin.detail.protocol_version')}
                       content={endpoint.protocolInformation.endpointProtocolVersion} 
                     />
                   </>
