d416b7b9490a78b9cf966320b85b4fdff199c243
Janine Semmler <janine.semmler@t-systems.com>
2022-05-05 22:28:24 +0200

    add current twin table information

--- cx-portal/src/components/pages/DigitalTwins/DigitalTwinsTableColumns.tsx
+++ cx-portal/src/components/pages/DigitalTwins/DigitalTwinsTableColumns.tsx
@@ -6,16 +6,29 @@ import { ShellDescriptor } from 'state/features/digitalTwins/types'
 // Columns definitions of Digital Twin page Data Grid
 export const DigitalTwinsTableColumns = ( translationHook: any, onDetailClick: (id: string) => void ): Array<GridColDef> => {
   const { t } = translationHook()
-  const columnIds = ['idShort', 'sm_count', 'asset_count']
 
   return [
-    ...columnIds.map(item => {return {
-      field: item,
-      headerName: t(`content.digitaltwin.table.columns.${item}`),
+    {
+      field: 'idShort',
+      headerName: t('content.digitaltwin.table.columns.idShort'),
+      flex: 2,
+    },
+    {
+      field: 'sm_count',
+      headerName: t('content.digitaltwin.table.columns.sm_count'),
       flex: 1,
-    }}),
+      align: 'center',
+      renderCell: ({ row }: { row: ShellDescriptor }) => <>{row.submodelDescriptors.length}</>
+    },
     {
       field: '',
+      headerName: t('content.digitaltwin.table.columns.asset_count'),
+      flex: 1,
+      align: 'center',
+      renderCell: ({ row }: { row: ShellDescriptor }) => <>{row.specificAssetIds.length}</>
+    },
+    {
+      field: 'detail',
       headerName: `Detail`,
       flex: 1,
       align: 'center',
