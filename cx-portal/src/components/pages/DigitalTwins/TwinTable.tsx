bffe45c62c7dc17d40563add2856fb78f2b0f644
Janine Semmler <janine.semmler@t-systems.com>
2022-05-06 00:13:54 +0200

    add pagination functionality

--- cx-portal/src/components/pages/DigitalTwins/TwinTable.tsx
+++ cx-portal/src/components/pages/DigitalTwins/TwinTable.tsx
@@ -1,23 +1,31 @@
-import { Table } from "cx-portal-shared-components";
-import { useEffect } from "react";
+import { Button, Table } from "cx-portal-shared-components";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next"
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDigitalTwins } from "state/features/digitalTwins/actions";
 import { twinsSelector } from "state/features/digitalTwins/slice";
+import { ShellDescriptor } from "state/features/digitalTwins/types";
 import { DigitalTwinsTableColumns } from "./DigitalTwinsTableColumns";
 
 interface TwinTableProps {
   onTwinSelect: (id: string) => void
 }
+
 const TwinTable = ({ onTwinSelect }: TwinTableProps ) => {
   const { t } = useTranslation();
   const dispatch = useDispatch()
-  const { twinList, loading, error } = useSelector(twinsSelector);
+  const { twinList, loading } = useSelector(twinsSelector);
+  const [twins, setTwins] = useState<ShellDescriptor[]>([]);
+  const [pageNumber, setPageNumber] = useState<number>(0);
   const rowCount = 10;
 
   useEffect(() => {
-    dispatch(fetchDigitalTwins({filter: { page: 1, pageSize: rowCount }}));
-  }, [dispatch]);
+    dispatch(fetchDigitalTwins({filter: { page: pageNumber, pageSize: rowCount }}));
+  }, [dispatch, pageNumber]);
+
+  useEffect(() => {
+    setTwins((prevTwins) => prevTwins.concat(twinList.items))
+  }, [twinList])
 
   const onSearch = (value: string) => {
     console.log(value);
@@ -28,8 +36,8 @@ const TwinTable = ({ onTwinSelect }: TwinTableProps ) => {
   return(
     <section>
       <Table
-        rowCount={rowCount}
-        rowsPerPageOptions={[10, 25, 50]}
+        rowsCount={twinList.totalItems}
+        hideFooter
         loading={loading}
         disableSelectionOnClick={true}
         title={t('content.digitaltwin.table.title')}
@@ -37,9 +45,20 @@ const TwinTable = ({ onTwinSelect }: TwinTableProps ) => {
           onSearch: onSearch,
         }}
         columns={columns}
-        rows={twinList.items}
+        rows={twins}
         getRowId={(row) => row.identification}
       />
+      <div className="load-more-button-container">
+        {twinList.totalPages !== twinList.currentPage &&
+          <Button
+            size="medium"
+            sx={{mt: 15}}
+            onClick={() => setPageNumber((prevState) => prevState + 1)}
+          >
+            {t('content.digitaltwin.table.load_button')}
+          </Button>
+        }
+      </div>
     </section>
   )
 }
