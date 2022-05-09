161c247c562b09d0f843454e0fc41d9ffd429e22
Feras Ahmad <feras.ahmad@raysono.com>
2022-05-09 10:51:14 +0200

    CPLP-762 remove div from user list content

--- cx-portal/src/components/pages/UserManagement/AddUserRightOverlay/UserListContent.tsx
+++ cx-portal/src/components/pages/UserManagement/AddUserRightOverlay/UserListContent.tsx
@@ -6,36 +6,34 @@ export default function UserListContent() {
   const { t } = useTranslation()
 
   return (
-    <div>
-      <Table
-        title={t('content.usermanagement.table.title')}
-        toolbar={{
-          onSearch: () => {
-            console.log('search user right function')
+    <Table
+      title={t('content.usermanagement.table.title')}
+      toolbar={{
+        onSearch: () => {
+          console.log('search user right function')
+        },
+        onFilter: () => {
+          console.log('filter user right function')
+        },
+      }}
+      columns={[
+        { field: 'name', headerName: t('global.field.name'), flex: 2 },
+        { field: 'email', headerName: t('global.field.email'), flex: 2 },
+        {
+          field: 'enabled',
+          headerName: t('global.field.status'),
+          flex: 1,
+          renderCell: ({ value: enabled }) => {
+            const label = enabled ? 'active' : 'inactive'
+            return (
+              <StatusTag color="label" label={t(`global.field.${label}`)} />
+            )
           },
-          onFilter: () => {
-            console.log('filter user right function')
-          },
-        }}
-        columns={[
-          { field: 'name', headerName: t('global.field.name'), flex: 2 },
-          { field: 'email', headerName: t('global.field.email'), flex: 2 },
-          {
-            field: 'enabled',
-            headerName: t('global.field.status'),
-            flex: 1,
-            renderCell: ({ value: enabled }) => {
-              const label = enabled ? 'active' : 'inactive'
-              return (
-                <StatusTag color="label" label={t(`global.field.${label}`)} />
-              )
-            },
-          },
-        ]}
-        rows={userList}
-        getRowId={(row: { [key: string]: string }) => row.id}
-        hideFooter
-      />
-    </div>
+        },
+      ]}
+      rows={userList}
+      getRowId={(row: { [key: string]: string }) => row.id}
+      hideFooter
+    />
   )
 }
