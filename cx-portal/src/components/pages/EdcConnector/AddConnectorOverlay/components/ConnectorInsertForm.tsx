de7887a3d833ffa79d59662319b472c05ef83ff8
fayyild <9d2d4f32efbd444ca1da36941dc568a5d74da0ac>
2022-05-12 20:23:46 +0200

    duplicated form inputs merged to one component

--- cx-portal/src/components/pages/EdcConnector/AddConnectorOverlay/components/ConnectorInsertForm.tsx
+++ cx-portal/src/components/pages/EdcConnector/AddConnectorOverlay/components/ConnectorInsertForm.tsx
@@ -4,6 +4,38 @@ import { Box, Grid, useTheme } from '@mui/material'
 import { Controller } from 'react-hook-form'
 import { Input } from 'cx-portal-shared-components'
 
+const ConnectorFormInput = ({
+  control,
+  trigger,
+  errors,
+  helperText,
+  label,
+  placeholder,
+  name,
+  rules,
+}: any) => {
+  return (
+    <Controller
+      render={({ field: { onChange, value } }) => (
+        <Input
+          error={!!errors[name]}
+          helperText={helperText}
+          label={label}
+          placeholder={placeholder}
+          onChange={(event) => {
+            trigger(name)
+            onChange(event)
+          }}
+          value={value}
+        />
+      )}
+      name={name}
+      control={control}
+      rules={rules}
+    />
+  )
+}
+
 // Pass Regex useless escape
 /* eslint-disable no-useless-escape */
 const ConnectorInsertForm = ({
@@ -28,85 +60,61 @@ const ConnectorInsertForm = ({
         >
           <form onSubmit={handleSubmit} className="form">
             <div className="form-input">
-              <Controller
-                render={({ field: { onChange, value } }) => (
-                  <Input
-                    error={!!errors['ConnectorName']}
-                    helperText={t(
-                      'content.edcconnector.modal.insertform.name.error'
-                    )}
-                    label={t(
-                      'content.edcconnector.modal.insertform.name.label'
-                    )}
-                    placeholder={t(
-                      'content.edcconnector.modal.insertform.name.placeholder'
-                    )}
-                    onChange={(event) => {
-                      trigger('ConnectorName')
-                      onChange(event)
-                    }}
-                    value={value}
-                  />
-                )}
-                name="ConnectorName"
-                control={control}
-                rules={{
-                  required: true,
-                }}
-              />
-            </div>
-
-            <div className="form-input">
-              <Controller
-                render={({ field: { onChange, value } }) => (
-                  <Input
-                    error={!!errors['ConnectorID']}
-                    helperText={t(
-                      'content.edcconnector.modal.insertform.id.error'
-                    )}
-                    label={t('content.edcconnector.modal.insertform.id.label')}
-                    placeholder={t(
-                      'content.edcconnector.modal.insertform.id.placeholder'
-                    )}
-                    onChange={(event) => {
-                      trigger('ConnectorID')
-                      onChange(event)
-                    }}
-                    value={value}
-                  />
-                )}
-                name="ConnectorID"
-                control={control}
-                rules={{
-                  required: true,
+              <ConnectorFormInput
+                {...{
+                  control,
+                  trigger,
+                  errors,
+                  name: 'ConnectorName',
+                  helperText: t(
+                    'content.edcconnector.modal.insertform.name.error'
+                  ),
+                  label: t('content.edcconnector.modal.insertform.name.label'),
+                  placeholder: t(
+                    'content.edcconnector.modal.insertform.name.placeholder'
+                  ),
                 }}
               />
             </div>
             <div className="form-input">
-              <Controller
-                render={({ field: { onChange, value } }) => (
-                  <Input
-                    error={!!errors['ConnectorURL']}
-                    helperText={t(
-                      'content.edcconnector.modal.insertform.url.error'
-                    )}
-                    label={t('content.edcconnector.modal.insertform.url.label')}
-                    placeholder={t(
-                      'content.edcconnector.modal.insertform.url.placeholder'
-                    )}
-                    onChange={(event) => {
-                      trigger('ConnectorURL')
-                      onChange(event)
-                    }}
-                    value={value}
-                  />
-                )}
-                name="ConnectorURL"
-                control={control}
-                rules={{
-                  required: true,
-                  pattern:
-                    /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w-]+)+[\w\-_~:/?#[\]@!&',;=.]+$/gm,
+              <ConnectorFormInput
+                {...{
+                  control,
+                  trigger,
+                  errors,
+                  name: 'ConnectorID',
+                  rules: {
+                    required: true,
+                  },
+                  helperText: t(
+                    'content.edcconnector.modal.insertform.id.error'
+                  ),
+                  label: t('content.edcconnector.modal.insertform.id.label'),
+                  placeholder: t(
+                    'content.edcconnector.modal.insertform.id.placeholder'
+                  ),
+                }}
+              />
+            </div>
+            <div className="form-input">
+              <ConnectorFormInput
+                {...{
+                  control,
+                  trigger,
+                  errors,
+                  name: 'ConnectorURL',
+                  rules: {
+                    required: true,
+                    pattern:
+                      /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w-]+)+[\w\-_~:/?#[\]@!&',;=.]+$/gm,
+                  },
+                  helperText: t(
+                    'content.edcconnector.modal.insertform.url.error'
+                  ),
+                  label: t('content.edcconnector.modal.insertform.url.label'),
+                  placeholder: t(
+                    'content.edcconnector.modal.insertform.url.placeholder'
+                  ),
                 }}
               />
             </div>
