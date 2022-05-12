5a78cde2b8305bfcf5338e7ae3b33c965565aef1
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-05-12 08:53:20 +0200

    added snackbar

---
+++ cx-portal-shared-components/src/components/basic/Snackbar/index.tsx
@@ -0,0 +1,37 @@
+import MuiSnackbar from '@mui/material/Snackbar'
+import MuiAlert, { AlertProps } from '@mui/material/Alert'
+import { forwardRef, useState } from 'react'
+
+interface SnackbarProps {
+  severity?: 'error' | 'warning' | 'info' | 'success'
+  children?: JSX.Element
+}
+
+const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+})
+
+export const Snackbar = ({ severity = 'info', children }: SnackbarProps) => {
+  const [open, setOpen] = useState(true)
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    setOpen(false)
+  }
+
+  return (
+    <MuiSnackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
+       {children}
+      </Alert>
+    </MuiSnackbar>
+  )
+}
