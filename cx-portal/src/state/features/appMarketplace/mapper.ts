7d5525f07e8ed479ec7c3c0fad863b11a047a013
Nidhi garg <nidhi.garg@partner.bmw.in>
2022-05-19 21:26:39 +0530

    fixed image gallery api issues

--- cx-portal/src/state/features/appMarketplace/mapper.ts
+++ cx-portal/src/state/features/appMarketplace/mapper.ts
@@ -1,15 +1,23 @@
-import { CardItems } from 'cx-portal-shared-components'
+import { CardItems, ImageType } from 'cx-portal-shared-components'
 import { getAssetBase } from 'services/EnvironmentService'
 import { AppMarketplaceApp } from './types'
 
 const baseAssets = getAssetBase()
 
-export const getAppImage = (app: AppMarketplaceApp): string =>
+export const getAppLeadImage = (app: AppMarketplaceApp): string =>
   !app.leadPictureUri || app.leadPictureUri === 'ERROR'
     ? `${baseAssets}/images/apps/default/lead.png`
     : app.leadPictureUri.startsWith('https://')
-    ? app.leadPictureUri
-    : `${baseAssets}/images/apps/${app.id}/${app.leadPictureUri}`
+      ? app.leadPictureUri
+      : `${baseAssets}/images/apps/${app.id}/${app.leadPictureUri}`
+
+export const getAppImage = (appid: string, image: string): ImageType => ({
+  text: "image caption",
+  url: image.startsWith('https://')
+    ? image
+    : `${baseAssets}/images/apps/${appid}/${image}`
+})
+
 
 export const appToCard = (app: AppMarketplaceApp): CardItems => ({
   ...app,
@@ -17,15 +25,15 @@ export const appToCard = (app: AppMarketplaceApp): CardItems => ({
   description: app.shortDescription === 'ERROR' ? '' : app.shortDescription,
   price: app.price === 'ERROR' ? '' : app.price,
   image: {
-    src: getAppImage(app),
+    src: getAppLeadImage(app),
     alt: app.title,
   },
   onClick: app.link
     ? () => {
-        // same tab
-        // document.location.href = app.link || '/notfound'
-        // new tab
-        window.open(app.link, '_blank')?.focus()
-      }
+      // same tab
+      // document.location.href = app.link || '/notfound'
+      // new tab
+      window.open(app.link, '_blank')?.focus()
+    }
     : undefined,
 })
