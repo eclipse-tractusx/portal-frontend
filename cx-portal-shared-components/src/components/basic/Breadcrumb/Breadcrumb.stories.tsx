3833de3d5221417061fef25f5d46bd06ed93fcb5
Feras Ahmad <feras.ahmad@raysono.com>
2022-05-20 15:42:28 +0200

    CPLP-661 add breadcrumb component

---
+++ cx-portal-shared-components/src/components/basic/Breadcrumb/Breadcrumb.stories.tsx
@@ -0,0 +1,52 @@
+import { Link, Typography } from '@mui/material'
+import { ComponentStory } from '@storybook/react'
+
+import { Breadcrumb as Component } from '.'
+
+export default {
+  title: 'Breadcrumb',
+  component: Component,
+  argTypes: {
+  },
+}
+
+const breadcrumbs = [
+  <Link
+    underline="hover"
+    key="1"
+    color="inherit"
+    sx={{cursor: 'pointer', fontSize:'14px'}}
+    onClick={() => console.log('navigate to home')}
+  >
+    Home
+  </Link>,
+  <Link
+    underline="hover"
+    key="2"
+    color="inherit"
+    sx={{cursor: 'pointer', fontSize:'14px'}}
+    onClick={() => console.log('navigate to Breackcrumb 1')}
+  >
+    Breadcrumb 1
+  </Link>,
+  <Typography
+    key="3"
+    color="text.primary"
+    sx={{ fontSize:'14px' }}
+  >
+    Breadcrumb 2
+  </Typography>,
+];
+
+const Template: ComponentStory<typeof Component> = (args: any) => (
+  <Component {...args} />
+)
+
+export const Breadcrumb = Template.bind({})
+Breadcrumb.args = {
+  backButton: true,
+  backButtonLabel: 'Back',
+  backButtonVariant: 'outlined',
+  onBackButtonClick: () => console.log('Back button clicked!'),
+  breadcrumbs: breadcrumbs,
+}
---
+++ cx-portal-shared-components/src/components/basic/Breadcrumb/index.tsx
@@ -0,0 +1,43 @@
+import { Box, Breadcrumbs } from '@mui/material'
+import { Button } from '../Button'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+interface BreadcrumbProps {
+  backButton?: boolean
+  backButtonLabel?: string
+  backButtonVariant?: "text" | "outlined" | "contained"
+  onBackButtonClick?: React.MouseEventHandler
+  breadcrumbs: any[]
+}
+
+export const Breadcrumb = ({
+  backButton,
+  backButtonLabel,
+  backButtonVariant='text',
+  onBackButtonClick,
+  breadcrumbs,
+ }: BreadcrumbProps) => { 
+  return (
+    <Box sx={{ display: 'flex' }}>
+      {backButton && (
+        <Button
+          onClick={onBackButtonClick}
+          color="secondary"
+          variant={`${backButtonVariant}`}
+          size="small"
+          sx={{ width: 'fit-content', marginRight: '24px', padding: '4px 12px !important' }}
+        >
+          <ArrowBackIcon sx={{marginRight: '5px'}} /> {backButtonLabel}
+        </Button>
+      )}
+
+      <Breadcrumbs
+        separator=" "
+        aria-label="breadcrumb"
+        sx={{ margin: 'auto 0px', hight: 'fit-content' }}
+      >
+        {breadcrumbs}
+      </Breadcrumbs>
+    </Box>
+  )
+}
