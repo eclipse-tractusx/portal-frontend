f3685901d35b8fe211bd3287059da44d95b8306a
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-05-24 08:33:10 +0200

    fixed sonar issues

--- cx-portal/src/state/features/appFavorites/slice.ts
+++ cx-portal/src/state/features/appFavorites/slice.ts
@@ -5,6 +5,20 @@ import { InitialListState, ListState, RequestState } from 'types/MainTypes'
 
 const initialState: ListState<string> = { ...InitialListState }
 
+const pendingCase = (state: ListState<string>, action: any) => ({
+  ...state,
+  change: action.meta.arg || null,
+  request: RequestState.SUBMIT,
+  error: '',
+})
+
+const rejectedCase = (state: ListState<string>, action: any) => ({
+  ...state,
+  change: null,
+  request: RequestState.ERROR,
+  error: action.error.message as string,
+})
+
 export const slice = createSlice({
   name: 'apps/favorites',
   initialState,
@@ -31,44 +45,24 @@ export const slice = createSlice({
     }))
 
     //add
-    builder.addCase(addItem.pending, (state, action) => ({
-      ...state,
-      change: action.meta.arg || null,
-      request: RequestState.SUBMIT,
-      error: '',
-    }))
-    builder.addCase(addItem.fulfilled, (state, action) => ({
+    builder.addCase(addItem.pending, pendingCase)
+    builder.addCase(addItem.fulfilled, (state) => ({
       items: [...state.items, state.change!],
       change: null,
       request: RequestState.OK,
       error: '',
     }))
-    builder.addCase(addItem.rejected, (state, action) => ({
-      ...state,
-      change: null,
-      request: RequestState.ERROR,
-      error: action.error.message as string,
-    }))
+    builder.addCase(addItem.rejected, rejectedCase)
 
     //remove
-    builder.addCase(removeItem.pending, (state, action) => ({
-      ...state,
-      change: action.meta.arg || null,
-      request: RequestState.SUBMIT,
-      error: '',
-    }))
+    builder.addCase(removeItem.pending, pendingCase)
     builder.addCase(removeItem.fulfilled, (state) => ({
       items: state.items.filter((a) => a !== state.change),
       change: null,
       request: RequestState.OK,
       error: '',
     }))
-    builder.addCase(removeItem.rejected, (state, action) => ({
-      ...state,
-      change: null,
-      request: RequestState.ERROR,
-      error: action.error.message as string,
-    }))
+    builder.addCase(removeItem.rejected, rejectedCase)
   },
 })
 
