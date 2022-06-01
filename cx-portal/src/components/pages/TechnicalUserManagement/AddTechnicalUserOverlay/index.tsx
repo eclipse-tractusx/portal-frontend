fefb987cde9ee02a329610c3c098f0ad04c8168a
floriansommerraysono <florian.sommer@raysonso.com>
2022-06-01 11:02:10 +0200

    Bugfix CPLP-793: Addin missing import

--- cx-portal/src/components/pages/TechnicalUserManagement/AddTechnicalUserOverlay/index.tsx
+++ cx-portal/src/components/pages/TechnicalUserManagement/AddTechnicalUserOverlay/index.tsx
@@ -14,6 +14,7 @@ import {
   Button,
 } from 'cx-portal-shared-components'
 import { useTranslation } from 'react-i18next'
+import { DialogContent } from 'cx-portal-shared-components/src'
 
 interface AddTechnicalUserOverlayProps {
   dialogOpen: boolean
