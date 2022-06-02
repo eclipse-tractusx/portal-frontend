5c9ffe75a91ee52d0ed610590dd77e110f2a5536
floriansommerraysono <florian.sommer@raysonso.com>
2022-06-02 17:37:03 +0200

    CPLP-793: Form validation for technical user overlay component

---
+++ cx-portal/src/components/pages/TechnicalUserManagement/AddTechnicalUserOverlay/components/TechnicalUserAddForm.tsx
@@ -0,0 +1,159 @@
+import Box from '@mui/material/Box'
+import { useTranslation } from 'react-i18next'
+import InputLabel from '@mui/material/InputLabel'
+import { Controller } from 'react-hook-form'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import FormHelperText from '@mui/material/FormHelperText'
+import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined'
+import '../AddTechnicalUserOverlay.scss'
+
+const TechnicalUserAddFormSelect = ({
+  control,
+  trigger,
+  errors,
+  name,
+  rules,
+}: any) => {
+  const { t } = useTranslation()
+
+  return (
+      <Controller
+        render={({ field: { onChange, value} }) => (
+          <>
+          <InputLabel error={!!errors[name]} sx={{ marginBottom: '7px' }}>
+            {t('content.addUser.technicalUser.addOverlay.service')}
+          </InputLabel>
+          <Select
+            error={!!errors[name]}
+            onChange={(event) => {
+              trigger(name)
+              onChange(event)
+            }}
+            value={value}
+            variant="filled"
+            fullWidth
+            sx={{
+              color: value === 'none' ? 'gray' : '',
+            }}
+          >
+            <MenuItem disabled value="none">{t('global.actions.pleaseSelect')}</MenuItem>
+            <MenuItem value="digitaltwin">Digital Twin</MenuItem>
+            <MenuItem value="semantichub">Semantic Hub</MenuItem>
+          </Select>
+            {!!errors[name] &&
+              <FormHelperText sx={{ marginBottom: '23px', color: 'danger.danger' }}>
+                {t('content.addUser.technicalUser.addOverlay.error.select')}
+              </FormHelperText>}
+          </>
+        )}
+        name={name}
+        control={control}
+        rules={rules}
+      />
+  )
+}
+
+const TechnicalUserAddFormTextfield = ({
+  control,
+  trigger,
+  errors,
+  helperText,
+  label,
+  placeholder,
+  name,
+  rules,
+}: any) => {
+  const CHARACTER_LIMIT = 120
+
+  return(
+    <Controller render={({field: { onChange, value}}) => (
+      <>
+      <InputLabel error={!!errors[name]} sx={{ marginBottom: '7px' }}>
+        {label}
+      </InputLabel>
+      <TextField
+        error={!!errors[name]}
+        fullWidth
+        helperText={!!errors[name] ? helperText : `${value.length}/${CHARACTER_LIMIT}`}
+        inputProps={{
+          maxLength: CHARACTER_LIMIT,
+        }}
+        multiline
+        onChange={(event) => {
+          trigger(name)
+          onChange(event)
+        }}
+        placeholder={placeholder}
+        value={value}
+        variant="filled"
+        FormHelperTextProps={{
+          sx: { marginLeft: !!errors[name] ? '' : 'auto' },
+        }}
+        InputProps={{
+          endAdornment: (!!errors[name] && (
+            <InputAdornment sx={{ color: 'danger.danger' }} position="end">
+              <ErrorOutlineOutlinedIcon />
+            </InputAdornment>
+          )),
+        }}
+      />
+      </>
+    )}
+    name={name}
+    control={control}
+    rules={rules}
+    />
+  )
+}
+
+export const TechnicalUserAddForm =  ({
+  control,
+  errors,
+  handleSubmit,
+  trigger,
+}: any) => {
+  const { t } = useTranslation()
+
+  return (
+    <Box sx={{ marginBottom: '23px' }}>
+      <form onSubmit={handleSubmit}>
+        <div className="form-input">
+          <TechnicalUserAddFormSelect
+            {...{
+              control,
+              errors,
+              trigger,
+              name: 'TechnicalUserService',
+              rules: {
+                required: true,
+                validate: (value: string) => value !== 'none'
+              },
+            }}
+          />
+        </div>
+        <div className="form-input">
+          <TechnicalUserAddFormTextfield
+            {...{
+              control,
+              errors,
+              trigger,
+              name: 'TechnicalUserDescription',
+              rules: {
+                required: true,
+              },
+              placeholder: t(
+                'content.addUser.technicalUser.addOverlay.description'
+              ),
+              label: t('content.addUser.technicalUser.addOverlay.description'),
+              helperText: t('content.addUser.technicalUser.addOverlay.error.description'),
+            }}
+          />
+        </div>
+
+      </form>
+    </Box>
+  )
+}
--- cx-portal/src/components/pages/TechnicalUserManagement/AddTechnicalUserOverlay/index.tsx
+++ cx-portal/src/components/pages/TechnicalUserManagement/AddTechnicalUserOverlay/index.tsx
@@ -1,26 +1,20 @@
-import { useState } from 'react'
 import Box from '@mui/material/Box'
-import InputLabel from '@mui/material/InputLabel'
-import Select from '@mui/material/Select'
-import MenuItem from '@mui/material/MenuItem'
-import TextField from '@mui/material/TextField'
-import InputAdornment from '@mui/material/InputAdornment'
-import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined'
-import FormHelperText from '@mui/material/FormHelperText'
 import {
+  Button,
   Dialog,
   DialogActions,
   DialogContent,
   DialogHeader,
-  Button,
   UserDetails,
 } from 'cx-portal-shared-components'
 import { useTranslation } from 'react-i18next'
+import { useForm } from 'react-hook-form'
+import { TechnicalUserAddForm } from './components/TechnicalUserAddForm'
 
 interface AddTechnicalUserOverlayProps {
   dialogOpen: boolean
   handleClose: React.MouseEventHandler
-  handleConfirm: React.MouseEventHandler
+  handleConfirm: () => void
 }
 
 export const AddTechnicalUserOverlay = ({
@@ -29,7 +23,6 @@ export const AddTechnicalUserOverlay = ({
   handleConfirm,
 }: AddTechnicalUserOverlayProps) => {
   const { t } = useTranslation()
-  const CHARACTER_LIMIT = 120
   const userDetailsData = [
     {
       cardCategory: 'Single Point of Contact (SPoC):',
@@ -41,13 +34,31 @@ export const AddTechnicalUserOverlay = ({
     },
   ]
 
-  const [values, setValues] = useState({
-    description: '',
-    service: 'none',
+  const defaultFormFieldValues = {
+    TechnicalUserService: 'none',
+    TechnicalUserDescription: '',
+  }
+
+  const {
+    handleSubmit,
+    getValues,
+    control,
+    trigger,
+    formState: { errors },
+  } = useForm({
+    defaultValues: defaultFormFieldValues,
+    mode: 'onChange',
   })
 
-  const handleChange = (name: string) => (event: any) => {
-    setValues({ ...values, [name]: event.target.value })
+  const onFormSubmit = async () => {
+    const validateFields = await trigger([
+      'TechnicalUserService',
+      'TechnicalUserDescription',
+    ])
+
+    if (validateFields) {
+      handleConfirm()
+    }
   }
 
   return (
@@ -57,61 +68,10 @@ export const AddTechnicalUserOverlay = ({
           title={t('content.addUser.technicalUserHeadline')}
           intro={t('content.addUser.technicalUserSubheadline')}
         />
-
         <DialogContent className="w-100">
-          <Box sx={{ marginBottom: '23px' }}>
-            <InputLabel error={true} sx={{ marginBottom: '7px' }}>
-              {t('content.addUser.technicalUser.addOverlay.service')}
-            </InputLabel>
-            <Select
-              value={values.service}
-              variant="filled"
-              fullWidth
-              error={true}
-              onChange={handleChange('service')}
-              sx={{
-                color: values.service === 'none' ? 'gray' : '',
-              }}
-            >
-              <MenuItem disabled value="none">
-                {t('global.actions.pleaseSelect')}
-              </MenuItem>
-              <MenuItem value="digitaltwin">Digital Twin</MenuItem>
-              <MenuItem value="semantichub">Semantic Hub</MenuItem>
-            </Select>
-            <FormHelperText sx={{ marginBottom: '23px', color: 'red' }}>
-              Error message!
-            </FormHelperText>
-
-            <InputLabel error={true} sx={{ marginBottom: '7px' }}>
-              {t('content.addUser.technicalUser.addOverlay.description')}
-            </InputLabel>
-            <TextField
-              variant="filled"
-              value={values.description}
-              placeholder={t(
-                'content.addUser.technicalUser.addOverlay.description'
-              )}
-              multiline
-              fullWidth
-              error={false}
-              FormHelperTextProps={{
-                sx: { marginLeft: 'auto' },
-              }}
-              helperText={`${values.description.length}/${CHARACTER_LIMIT}`}
-              onChange={handleChange('description')}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment sx={{ color: 'red' }} position="end">
-                    <ErrorOutlineOutlinedIcon />
-                  </InputAdornment>
-                ),
-              }}
-              inputProps={{
-                maxLength: CHARACTER_LIMIT,
-              }}
-            />
-          </Box>
+          <TechnicalUserAddForm
+            {...{ handleSubmit, control, errors, trigger }}
+          />
           <Box>
             <UserDetails
               columns={1}
@@ -120,12 +80,11 @@ export const AddTechnicalUserOverlay = ({
             />
           </Box>
         </DialogContent>
-
         <DialogActions>
           <Button variant="outlined" onClick={handleClose}>
             {`${t('global.actions.cancel')}`}
           </Button>
-          <Button variant="contained" onClick={handleConfirm}>
+          <Button variant="contained" onClick={onFormSubmit}>
             {`${t('global.actions.confirm')}`}
           </Button>
         </DialogActions>
