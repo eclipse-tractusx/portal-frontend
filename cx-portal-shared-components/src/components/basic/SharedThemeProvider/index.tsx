39b2f8af8c9f8bf7b4cd44167d520b75334a8d10
fayyild <9d2d4f32efbd444ca1da36941dc568a5d74da0ac>
2022-06-13 17:31:32 +0200

    CircularProgress sass styling changed global.scss file created and math function moved to there rollup error fixed

--- cx-portal-shared-components/src/components/basic/SharedThemeProvider/index.tsx
+++ cx-portal-shared-components/src/components/basic/SharedThemeProvider/index.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@mui/material/styles'
 import { theme } from '../../../theme'
 import '../../../scss/fonts.scss'
+import '../../../scss/global.scss'
 
 interface SharedThemeProviderProps {
   children: React.ReactNode
