85208056c58109e95583108e705d6b0b333ecbab
Janine Semmler <janine.semmler@t-systems.com>
2022-06-29 09:32:41 +0200

    code review fixes

--- cx-portal/src/components/pages/SemanticHub/SemanticModelTableColumn.tsx
+++ cx-portal/src/components/pages/SemanticHub/SemanticModelTableColumn.tsx
@@ -10,13 +10,13 @@ export const SemanticModelTableColumns = (
 ): Array<GridColDef> => {
   const columnIds = ['name', 'status', 'type', 'version', 'src']
   return [
-    ...columnIds.map((item) => {
-      return {
+    ...columnIds.map((item) =>
+      ({
         field: item,
         flex: item === 'name' || item === 'status' ? 3 : 2,
         headerName: t(`content.semantichub.table.columns.${item}`),
-      }
-    }),
+      })
+    ),
     {
       field: 'detail',
       headerName: `Detail`,
