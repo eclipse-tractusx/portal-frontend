1c47969bec51a905f5cb2eabb214c6f75e0427ce
Feras Ahmad <feras.ahmad@raysono.com>
2022-07-22 17:11:00 +0200

    CPLP-509 fix linter

--- cx-portal/src/components/shared/frame/PageBreadcrumb/PageBreadcrumb.tsx
+++ cx-portal/src/components/shared/frame/PageBreadcrumb/PageBreadcrumb.tsx
@@ -9,7 +9,7 @@ interface PageBreadcrumbProps {
 }
 
 function isParamsEmpty(params: object) {
-  for(let param in params) {
+  for (let param in params) {
     if (param) return false
   }
   return true
@@ -31,7 +31,7 @@ export const PageBreadcrumb = ({
         pathNames.splice(index, 1)
       }
     })
-    
+
     // no need to add params to breadcrumbs:
     if (!isParamsEmpty(params)) {
       pathNames.splice(pathNames.length - 1, 1)
@@ -46,14 +46,18 @@ export const PageBreadcrumb = ({
   const breadcrumbs = () => {
     const crumbsArr: any[] = []
     crumbs.forEach((item: string, index) => {
-      const count = crumbs.length -1
+      const count = crumbs.length - 1
       const navigatePage = count - index
       if (index === count) {
         // last item in breadcrumb has noch navigation
         crumbsArr.push(
-          <Typography key={index} color="text.primary" sx={{ fontSize: '14px' }}>
+          <Typography
+            key={index}
+            color="text.primary"
+            sx={{ fontSize: '14px' }}
+          >
             {getCrumbTitle(item)}
-          </Typography>,
+          </Typography>
         )
       } else {
         // other items in breadcrumbs has navigation
