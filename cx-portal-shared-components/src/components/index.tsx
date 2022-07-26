26816b0566033d9ccc82a7e6e1748e8f3ab87e7a
Feras Ahmad <feras.ahmad@raysono.com>
2022-07-26 08:29:21 +0200

    CPLP-809 export stepper component

--- cx-portal-shared-components/src/components/index.tsx
+++ cx-portal-shared-components/src/components/index.tsx
@@ -43,6 +43,7 @@ export { ErrorPage } from './basic/ErrorPage'
 export { MultiSelectList } from './basic/MultiSelectList'
 export { ProcessList } from './basic/ProcessList'
 export { CircleProgress } from './basic/Progress/CircleProgress'
+export { Stepper } from './basic/Stepper'
 
 export { Cards } from './content/Cards'
 export { Card } from './content/Cards/Card'
