f5bd770ddedf903008e28679b1af976c6e68ae03
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-08-02 09:47:21 +0200

    restructure techuser pages

--- cx-portal/src/components/pages/TechnicalUserManagement/TechnicalUserTable/index.tsx
+++ cx-portal/src/components/pages/TechnicalUserManagement/TechnicalUserTable/index.tsx
@@ -1,22 +1,35 @@
-import { IconButton, Table } from 'cx-portal-shared-components'
-import { useEffect } from 'react'
+/********************************************************************************
+ * Copyright (c) 2021,2022 BMW Group AG
+ * Copyright (c) 2021,2022 Contributors to the CatenaX (ng) GitHub Organisation.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+import { IconButton, PageLoadingTable } from 'cx-portal-shared-components'
 import { useTranslation } from 'react-i18next'
-import { useDispatch, useSelector } from 'react-redux'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 import { useNavigate } from 'react-router-dom'
-import { fetchPage } from 'features/admin/service/actions'
-import { itemsSelector } from 'features/admin/service/slist'
-import { ServiceAccount } from 'features/admin/service/types'
+import {
+  ServiceAccountListEntry,
+  useFetchServiceAccountListQuery,
+} from 'features/admin/service/apiSlice'
 
 export const TechnicalUserTable = () => {
   const { t } = useTranslation()
-  const dispatch = useDispatch()
   const navigate = useNavigate()
-  const serviceAccounts = useSelector(itemsSelector)
-
-  useEffect(() => {
-    dispatch(fetchPage(0))
-  }, [dispatch])
 
   const onUserDetailsClick = (userId: string, name: string) => {
     navigate('/usermanagement/technicaluser/userdetails/' + userId, {
@@ -26,13 +39,9 @@ export const TechnicalUserTable = () => {
 
   return (
     <div style={{ paddingTop: '30px' }}>
-      <Table
+      <PageLoadingTable<ServiceAccountListEntry>
         title={t('content.usermanagement.technicalUser.tableHeader')}
-        toolbar={{
-          onSearch: () => {
-            console.log('search function')
-          },
-        }}
+        loadLabel={t('global.more')}
         columns={[
           {
             field: 'name',
@@ -48,7 +57,7 @@ export const TechnicalUserTable = () => {
             field: 'details',
             headerName: t('global.field.details'),
             flex: 1,
-            renderCell: ({ row }: { row: ServiceAccount }) => (
+            renderCell: ({ row }: { row: ServiceAccountListEntry }) => (
               <IconButton
                 color="secondary"
                 onClick={() =>
@@ -60,10 +69,8 @@ export const TechnicalUserTable = () => {
             ),
           },
         ]}
-        rows={serviceAccounts}
+        fetch={useFetchServiceAccountListQuery}
         getRowId={(row: { [key: string]: string }) => row.serviceAccountId}
-        disableColumnMenu
-        hideFooter
       />
     </div>
   )
