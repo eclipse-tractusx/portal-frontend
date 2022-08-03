ece9a334187d39b51eb2d9fac07fa5f113d1d411
Nidhi garg <nidhi.garg@partner.bmw.in>
2022-08-03 17:46:42 +0530

    add roles dynamic in overlay

--- cx-portal/src/components/pages/UserManagement/AddUserOverlay/UserRoles.tsx
+++ cx-portal/src/components/pages/UserManagement/AddUserOverlay/UserRoles.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Checkbox } from 'cx-portal-shared-components'
 import { Box } from '@mui/material'
 import SubHeaderTitle from 'components/shared/frame/SubHeaderTitle'
 import { setUserRolesToAdd } from 'features/admin/user/actions'
+import { useParams } from 'react-router-dom'
+import { fetchRoles } from 'features/admin/approle/actions'
+import { roleSelector } from 'features/admin/approle/slice'
 
 interface UserRolesProps {
   headline?: string
@@ -13,44 +16,26 @@ interface UserRolesProps {
 export const UserRoles = ({ headline }: UserRolesProps) => {
   const { t } = useTranslation()
   let [roles, setRoles] = useState<string[]>([])
+  const userRoles = useSelector(roleSelector)
+  const appId = useParams().appId
   const dispatch = useDispatch()
-  const userRoles = [
-    {
-      id: 'itAdmin',
-      title: t('global.userRoles.itAdmin'),
-    },
-    {
-      id: 'businessAdmin',
-      title: t('global.userRoles.businessAdmin'),
-    },
-    {
-      id: 'cxUser',
-      title: t('global.userRoles.cxUser'),
-    },
-    {
-      id: 'dataSpecialist',
-      title: t('global.userRoles.dataSpecialist'),
-    },
-    {
-      id: 'appDeveloper',
-      title: t('global.userRoles.appDeveloper'),
-    },
-    {
-      id: 'appAdmin',
-      title: t('global.userRoles.appAdmin'),
-    },
-  ]
+
+  useEffect(() => {
+    if (appId) {
+      dispatch(fetchRoles(appId))
+    }
+  }, [dispatch, appId])
 
   useEffect(() => {
     dispatch(setUserRolesToAdd(roles))
   }, [roles, dispatch])
 
-  const onInputChange = ({ e, title }: { e: any; title: string }) => {
-    if (!roles.includes(title) && e.target.checked) {
-      setRoles([...roles, title])
+  const onInputChange = ({ e, role }: { e: any; role: string }) => {
+    if (!roles.includes(role) && e.target.checked) {
+      setRoles([...roles, role])
     } else {
       const oldRoles = [...roles]
-      oldRoles.splice(oldRoles.indexOf(title), 1)
+      oldRoles.splice(oldRoles.indexOf(role), 1)
       setRoles([...oldRoles])
     }
   }
@@ -74,11 +59,11 @@ export const UserRoles = ({ headline }: UserRolesProps) => {
       )}
 
       <div className="checkbox-section">
-        {userRoles.map(({ title, id }) => (
+        {userRoles.map(({ roleId, role }) => (
           <Checkbox
-            label={title}
-            key={id}
-            onChange={(e) => onInputChange({ e, title })}
+            label={role}
+            key={roleId}
+            onChange={(e) => onInputChange({ e, role })}
           />
         ))}
       </div>
