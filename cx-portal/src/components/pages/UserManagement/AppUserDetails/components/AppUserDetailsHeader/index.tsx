5ec9965c47d58d3be7ed94319e0b19d59d293f8b
Nidhi garg <nidhi.garg@partner.bmw.in>
2022-08-03 12:21:59 +0530

    fixed error message UI for usermanagement appdetails page

--- cx-portal/src/components/pages/UserManagement/AppUserDetails/components/AppUserDetailsHeader/index.tsx
+++ cx-portal/src/components/pages/UserManagement/AppUserDetails/components/AppUserDetailsHeader/index.tsx
@@ -1,15 +1,20 @@
-import { Typography } from 'cx-portal-shared-components'
+import { useTranslation } from 'react-i18next'
+import { Typography, PageNotifications } from 'cx-portal-shared-components'
 import SubHeaderTitle from 'components/shared/frame/SubHeaderTitle'
 import { AppRole } from 'features/admin/approle/types'
 import './AppUserDetailsHeader.scss'
 
 export interface AppUserDetailsHeaderProps {
   roles: AppRole[]
+  error: string
 }
 
 export default function AppUserDetailsHeader({
   roles,
+  error,
 }: AppUserDetailsHeaderProps) {
+  const { t } = useTranslation()
+
   return (
     <section className="app-user-details-header">
       <div className="app-user-details-header-title">
@@ -27,20 +32,37 @@ export default function AppUserDetailsHeader({
       </div>
 
       <div className="app-user-details-header-roles-section">
-        {roles.map((role) => {
-          return (
-            <div key={role.role} className="app-user-details-header-role">
-              <Typography variant="h5">{role.role}</Typography>
-              <Typography
-                variant="h6"
-                className="app-user-details-header-description"
-              >
-                {role.description}
-              </Typography>
-            </div>
-          )
-        })}
+        {roles &&
+          roles.map((role) => {
+            return (
+              <div key={role.role} className="app-user-details-header-role">
+                <Typography variant="h5">{role.role}</Typography>
+                <Typography
+                  variant="h6"
+                  className="app-user-details-header-description"
+                >
+                  {role.description}
+                </Typography>
+              </div>
+            )
+          })}
       </div>
+      {
+        <div className="errorMsg">
+          {error && (
+            <PageNotifications
+              description={t(
+                'content.usermanagement.appUserDetails.roles.error.message'
+              )}
+              open
+              severity="error"
+              title={t(
+                'content.usermanagement.appUserDetails.roles.error.title'
+              )}
+            />
+          )}
+        </div>
+      }
     </section>
   )
 }
