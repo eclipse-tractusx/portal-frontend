aaa27e24cba51f676a203196cd095ab55499ba51
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-08-09 11:28:07 +0200

    sonar findings

--- cx-portal/src/components/pages/TechnicalUserManagement/AddTechnicalUserOverlay/index.tsx
+++ cx-portal/src/components/pages/TechnicalUserManagement/AddTechnicalUserOverlay/index.tsx
@@ -1,9 +1,3 @@
-interface AddTechnicalUserOverlayProps {
-  dialogOpen: boolean
-  handleClose: React.MouseEventHandler
-  handleConfirm: (formValues: DefaultFormFieldValuesType) => void
-}
-
 export type DefaultFormFieldValuesType = {
   TechnicalUserName: string
   TechnicalUserService: string
