7e7b531f98e098956091e64a425b34058de145d2
Deep <deep.patel@smartsensesolutions.com>
2022-09-20 14:18:43 +0530

    CPLP-1467-registry-search-ui-changes

--- cx-portal/src/components/pages/DigitalTwins/DigitalTwinDetailDialog.tsx
+++ cx-portal/src/components/pages/DigitalTwins/DigitalTwinDetailDialog.tsx
@@ -22,22 +22,19 @@ import {
   Dialog,
   DialogContent,
   DialogHeader,
-  Typography,
 } from 'cx-portal-shared-components'
 import { useSelector } from 'react-redux'
 import { twinsSelector } from 'features/digitalTwins/slice'
 import { TwinDetails } from './TwinDetails'
 import { Box, useTheme, CircularProgress } from '@mui/material'
-import { useTranslation } from 'react-i18next'
 
 interface TwinDialogProps {
   show: boolean
   onClose: () => void
 }
 const DigitalTwinDetailDialog = ({ show, onClose }: TwinDialogProps) => {
-  const { twin, loading, error } = useSelector(twinsSelector)
+  const { twin, loading } = useSelector(twinsSelector)
   const theme = useTheme()
-  const { t } = useTranslation()
 
   return (
     <Dialog open={show}>
@@ -53,13 +50,6 @@ const DigitalTwinDetailDialog = ({ show, onClose }: TwinDialogProps) => {
               }}
             />
           </Box>
-        )}
-        {error && (
-          <>
-            <Typography variant="h5" sx={{ mb: 4 }}>
-              {t('content.digitaltwin.detail.error')}
-            </Typography>
-          </>
         )}
       </DialogContent>
     </Dialog>
