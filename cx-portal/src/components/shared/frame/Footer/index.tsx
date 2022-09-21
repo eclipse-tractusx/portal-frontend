2a0859d0f227984fd757a67bfe39f3a725a54afc
Ronak Thacker <ronak.thacker@smartsensesolutions.com>
2022-09-21 19:33:43 +0530

    [CPLP-800] fixed footer color

--- cx-portal/src/components/shared/frame/Footer/index.tsx
+++ cx-portal/src/components/shared/frame/Footer/index.tsx
@@ -23,10 +23,13 @@ import { Navigation, IconButton } from 'cx-portal-shared-components'
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
 import { useTranslation } from 'react-i18next'
 import { NavLink } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import './Footer.scss'
+import { PAGES } from 'types/Constants'
 
 export const Footer = ({ pages }: { pages: string[] }) => {
   const { t } = useTranslation()
+  const location = useLocation()
   const items = pages.map((page) => ({
     to: page,
     title: t(`pages.${page}`),
@@ -37,6 +40,9 @@ export const Footer = ({ pages }: { pages: string[] }) => {
       behavior: 'smooth',
     })
   }
+  const isAppOverviewPage = [PAGES.APPOVERVIEW, PAGES.APP_MANAGEMENT, PAGES.APPRELEASEPROCESS].find(
+    (e) => location.pathname.split('/').includes(e)
+  )
 
   const [showScrollToTop, setShowScrollToTop] = useState(false)
 
@@ -49,7 +55,7 @@ export const Footer = ({ pages }: { pages: string[] }) => {
   }, [])
 
   return (
-    <footer>
+    <footer style={{ background: isAppOverviewPage ? '#F9F9F9' : '' }}>
       {showScrollToTop && (
         <IconButton
           color="secondary"
