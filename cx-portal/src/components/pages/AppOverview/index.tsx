201ade5902a4a6790f7881b4196a3dddd4df4c61
Ronak Thacker <ronak.thacker@smartsensesolutions.com>
2022-09-21 19:21:54 +0530

    [CPLP-800] design added for app overview page

--- cx-portal/src/components/pages/AppOverview/index.tsx
+++ cx-portal/src/components/pages/AppOverview/index.tsx
@@ -18,14 +18,139 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-import { PageHeader } from 'cx-portal-shared-components'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { PageBreadcrumb } from 'components/shared/frame/PageBreadcrumb/PageBreadcrumb'
+import {
+  PageHeader,
+  Typography,
+  Cards,
+  ViewSelector,
+  SearchInput,
+  CardAddService,
+  CardHorizontal,
+} from 'cx-portal-shared-components'
+import { appCardStatus } from 'features/apps/mapper'
+import { Box } from '@mui/material'
+import './AppOverview.scss'
+import { useFetchActiveAppsQuery } from 'features/apps/apiSlice'
+import uniqueId from 'lodash/uniqueId'
 
 export default function AppOverview() {
   const { t } = useTranslation()
+  const [group, setGroup] = useState<string>('')
+  const { data } = useFetchActiveAppsQuery() //// TODO: Replace to Recently changed apps api
+  const items = appCardStatus(data!)
+
+  const setView = (e: React.MouseEvent<HTMLInputElement>) => {
+    setGroup(e.currentTarget.value)
+  }
+
+  const categoryViews = [
+    {
+      buttonText: t('content.appoverview.filter.all'),
+      buttonValue: '',
+      onButtonClick: setView,
+    },
+    {
+      buttonText: t('content.appoverview.filter.active'),
+      buttonValue: 'useCases',
+      onButtonClick: setView,
+    },
+    {
+      buttonText: t('content.appoverview.filter.inactive'),
+      buttonValue: 'useCases',
+      onButtonClick: setView,
+    },
+    {
+      buttonText: t('content.appoverview.filter.wip'),
+      buttonValue: 'useCases',
+      onButtonClick: setView,
+    },
+  ]
+
   return (
-    <main>
-      <PageHeader title={t('pages.appoverview')} />
-    </main>
+    <div className="appoverview-app">
+      <PageHeader
+        title={t('content.appoverview.headerTitle')}
+        topPage={true}
+        headerHeight={200}
+      >
+        <PageBreadcrumb backButtonVariant="contained" />
+      </PageHeader>
+      <div className="desc-recently">
+        <div className="container">
+          <Typography variant="h4" className="desc-heading">
+            {t('content.appoverview.recently.header')}
+          </Typography>
+          <Typography variant="body2" className="desc-message">
+            {t('content.appoverview.recently.subheader')}
+          </Typography>
+          <div className="desc-card">
+            {items ? (
+              <Cards
+                items={items.slice(0, 4)}
+                columns={4}
+                buttonText="Details"
+                variant="minimal"
+                filledBackground={true}
+              />
+            ) : null}
+          </div>
+        </div>
+      </div>
+      <div className="app-main">
+        <Box sx={{ marginTop: '20px' }} className="overview-section">
+          <section className="overview-section-content">
+            <Typography
+              sx={{ fontFamily: 'LibreFranklin-Light' }}
+              variant="h3"
+              className="section-title"
+            >
+              {t('content.appoverview.title')}
+            </Typography>
+
+            <Box sx={{ textAlign: 'center' }}>
+              <SearchInput
+                sx={{ minWidth: '544px' }}
+                margin={'normal'}
+                placeholder={t('content.appoverview.inputPlaceholder')}
+              />
+            </Box>
+            <div className="view-selector">
+              <ViewSelector activeView={group} views={categoryViews} />
+            </div>
+          </section>
+        </Box>
+
+        <div className="app-detail">
+          <div className="app-child">
+            <CardAddService
+              borderRadius={20}
+              backgroundColor={'background.background01'}
+              onButtonClick={function noRefCheck() {}}
+              title={t('content.appoverview.addbtn')}
+            />
+          </div>
+          {items?.map((item: any) => {
+            return (
+              <div className="app-child" key={uniqueId(item.title)}>
+                <CardHorizontal
+                  borderRadius={20}
+                  imageAlt={item.image.alt || ''}
+                  imagePath={item.image.src}
+                  label={item.subtitle || ''}
+                  onBtnClick={function noRefCheck() {}}
+                  statusText={item.statusText}
+                  status={item.status}
+                  title={item.title}
+                  backgroundColor={'background.background01'}
+                />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
   )
 }
