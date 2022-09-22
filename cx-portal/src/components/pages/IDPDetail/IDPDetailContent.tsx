d00a0009968dc135cdee9cb08d823eb6de612ad3
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-09-22 13:41:13 +0200

    add and update IDP details

--- cx-portal/src/components/pages/IDPDetail/IDPDetailContent.tsx
+++ cx-portal/src/components/pages/IDPDetail/IDPDetailContent.tsx
@@ -20,40 +20,81 @@
 
 import { Input } from 'cx-portal-shared-components'
 import { IdentityProvider, IDPAuthType } from 'features/admin/idpApiSlice'
+import { editIDPSelector, FORMS, storeForm } from 'features/control/formSlice'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
 import './style.scss'
+
+const EditInput = ({ name }: { name: string }) => {
+  const { t } = useTranslation('', { keyPrefix: 'content.idpdetail' })
+  const dispatch = useDispatch()
+  const fieldValue = useSelector(editIDPSelector)
+
+  const checkField = (e: any) => {
+    let att: { [name: string]: string } = {}
+    att[e.target.name] = e.target.value
+    dispatch(storeForm({ form: FORMS.IDP_FORM, att }))
+  }
+
+  return (
+    <Input
+      name={name}
+      label={t(name)}
+      value={fieldValue[name] || ''}
+      onChange={checkField}
+    />
+  )
+}
 
 export default function IDPDetailContent({ idp }: { idp: IdentityProvider }) {
   const { t } = useTranslation('', { keyPrefix: 'content.idpdetail' })
+  const dispatch = useDispatch()
   const authType = idp.saml ? IDPAuthType.SAML : IDPAuthType.OIDC
   const authTypeData = idp.oidc || idp.saml
   const state = idp.enabled ? 'enabled' : 'disabled'
 
-  const Field = ({ name, value }: { name: string; value: string }) => (
-    <div style={{ margin: '12px 0' }}>
-      <Input label={name} value={value} disabled={true} />
-    </div>
-  )
+  useEffect(() => {
+    let att: { [name: string]: string } = {}
+    att.displayName = idp.displayName || ''
+    att.metadataUrl = ''
+    att.clientId = authTypeData?.clientId || ''
+    att.secret = ''
+    att.clientAuthMethod = authTypeData?.clientAuthMethod || ''
+    att.signatureAlgorithm = authTypeData?.signatureAlgorithm || ''
+    dispatch(storeForm({ form: FORMS.IDP_FORM, att }))
+    // eslint-disable-next-line
+  }, [idp])
 
   return (
     <div className="idp-detail-content">
+      <Input label={'ID'} value={idp.identityProviderId} disabled={true} />
       <div className="idp-name-row">
-        <Field name={t('alias')} value={idp.alias} />
-        <Field name={t('displayName')} value={idp.displayName || ''} />
+        <Input label={t('alias')} value={idp.alias} disabled={true} />
+        <EditInput name="displayName" />
       </div>
       <div className="idp-name-row">
-        <Field name={t('authType')} value={authType} />
-        <Field name={t('status')} value={state} />
+        <Input label={t('authType')} value={authType} disabled={true} />
+        <Input label={t('status')} value={state} disabled={true} />
       </div>
-      <Field name={t('authURL')} value={authTypeData?.authorizationUrl || ''} />
-      <Field
-        name={t('authMethod')}
-        value={authTypeData?.clientAuthMethod || ''}
+      <Input
+        label={t('authorizationUrl')}
+        value={authTypeData?.authorizationUrl || ''}
+        disabled={true}
       />
+      <EditInput name="metadataUrl" />
+      <div className="idp-name-row">
+        <EditInput name="clientId" />
+        <EditInput name="secret" />
+      </div>
+      <div className="idp-name-row">
+        <EditInput name="clientAuthMethod" />
+        <EditInput name="signatureAlgorithm" />
+      </div>
       <ul>
         {idp.mappers.map((mapper) => (
           <li key={mapper.id}>
-            <Field name={mapper.type} value={mapper.name} />
+            <Input label={mapper.type} value={mapper.name} disabled={true} />
           </li>
         ))}
       </ul>
