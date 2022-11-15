0159f5496bec9f4f7e76c77c4d1eec804311ca97
Nidhi garg <nidhi.garg@partner.bmw.in>
2022-11-15 12:32:07 +0530

    revert changes in env file

--- cx-portal/src/services/EnvironmentService.ts
+++ cx-portal/src/services/EnvironmentService.ts
@@ -18,90 +18,90 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-const LOCAL_SERVICES_FRONTEND = 'https://portal.dev.demo.catena-x.net'
-const LOCAL_SERVICES_BACKEND = 'https://portal-backend.dev.demo.catena-x.net'
-//const LOCAL_SERVICES_CENTRALIDP = 'https://centralidp.dev.demo.catena-x.net'
-//const LOCAL_SERVICES_BPDM = 'https://bpdm.dev.demo.catena-x.net'
-
-export const getHostname = () => window.location.hostname
-
-export const isLocal = () => getHostname() === 'localhost'
-
-export const getApiBase = () =>
-  isLocal()
-    ? LOCAL_SERVICES_BACKEND
-    : window.location.origin.replace('portal', 'portal-backend')
-
-export const getAssetBase = () =>
-  `${isLocal() ? LOCAL_SERVICES_FRONTEND : ''}/assets`
-
-export const getCentralIdp = () => {
-  const hostname = getHostname()
-  if (hostname === 'portal.dev.demo.catena-x.net')
-    return 'https://centralidp.demo.catena-x.net/auth'
-  if (hostname === 'portal-pen.dev.demo.catena-x.net')
-    return 'https://centralidp-pen.dev.demo.catena-x.net/auth'
-  if (hostname === 'portal.beta.demo.catena-x.net')
-    return 'https://centralidp.beta.demo.catena-x.net/auth'
-  if (hostname === 'portal.pre-prod.demo.catena-x.net')
-    return 'https://centralidp.pre-prod.demo.catena-x.net/auth'
-  if (hostname === 'portal.catena-x.net')
-    return 'https://centralidp.catena-x.net/auth'
-  return 'https://centralidp.dev.demo.catena-x.net/auth'
-}
-
-export const getClientId = () => 'Cl2-CX-Portal'
-
-//TODO: remove hard coded url and activate after setup of BPDM Api
-export const getBpdmApiBase = () => {
-  const hostname = getHostname()
-  if (hostname === 'portal.dev.demo.catena-x.net')
-    return 'https://partners-pool.dev.demo.catena-x.net/api'
-  if (hostname === 'portal-pen.dev.demo.catena-x.net')
-    return 'https://partners-pool-pen.dev.demo.catena-x.net/api'
-  if (hostname === 'portal.beta.demo.catena-x.net')
-    return 'https://partners-pool.beta.demo.catena-x.net/api'
-  if (hostname === 'portal.pre-prod.demo.catena-x.net')
-    return 'https://partners-pool.pre-prod.demo.catena-x.net/api'
-  if (hostname === 'portal.catena-x.net')
-    return 'https://partners-pool.catena-x.net/api'
-  return 'https://partners-pool.dev.demo.catena-x.net/api'
-}
-//export const getBpdmApiBase = () =>
-//  isLocal()
-//    ? LOCAL_SERVICES_BPDM
-//    : window.location.origin.replace('portal', 'bpdm')
-
-export const getSemanticApiBase = () => {
-  const hostname = getHostname()
-  if (hostname === 'portal.dev.demo.catena-x.net')
-    return 'https://semantics.dev.demo.catena-x.net/'
-  if (hostname === 'portal-pen.dev.demo.catena-x.net')
-    return 'https://semantics-pen.dev.demo.catena-x.net/'
-  if (hostname === 'portal.beta.demo.catena-x.net')
-    return 'https://semantics.beta.demo.catena-x.net/'
-  if (hostname === 'portal.pre-prod.demo.catena-x.net')
-    return 'https://semantics.pre-prod.demo.catena-x.net/'
-  if (hostname === 'portal.catena-x.net')
-    return 'https://semantics.catena-x.net/'
-  return 'https://semantics.dev.demo.catena-x.net/'
-}
-
-export const getClientIdSemantic = () => 'Cl3-CX-Semantic'
-
-export const getClientIdDigitalTwin = () => 'Cl4-CX-DigitalTwin'
-
-const EnvironmentService = {
-  isLocal,
-  getHostname,
-  getApiBase,
-  getAssetBase,
-  getBpdmApiBase,
-  getCentralIdp,
-  getSemanticApiBase,
-  getClientId,
-  getClientIdSemantic,
-  getClientIdDigitalTwin,
-}
-
-export default EnvironmentService
+ const LOCAL_SERVICES_FRONTEND = 'https://portal.dev.demo.catena-x.net'
+ const LOCAL_SERVICES_BACKEND = 'https://portal-backend.dev.demo.catena-x.net'
+ //const LOCAL_SERVICES_CENTRALIDP = 'https://centralidp.dev.demo.catena-x.net'
+ //const LOCAL_SERVICES_BPDM = 'https://bpdm.dev.demo.catena-x.net'
+ 
+ export const getHostname = () => window.location.hostname
+ 
+ export const isLocal = () => getHostname() === 'localhost'
+ 
+ export const getApiBase = () =>
+   isLocal()
+     ? LOCAL_SERVICES_BACKEND
+     : window.location.origin.replace('portal', 'portal-backend')
+ 
+ export const getAssetBase = () =>
+   `${isLocal() ? LOCAL_SERVICES_FRONTEND : ''}/assets`
+ 
+ export const getCentralIdp = () => {
+   const hostname = getHostname()
+   if (hostname === 'portal.int.demo.catena-x.net')
+     return 'https://centralidp.demo.catena-x.net/auth'
+   if (hostname === 'portal-pen.dev.demo.catena-x.net')
+     return 'https://centralidp-pen.dev.demo.catena-x.net/auth'
+   if (hostname === 'portal.beta.demo.catena-x.net')
+     return 'https://centralidp.beta.demo.catena-x.net/auth'
+   if (hostname === 'portal.pre-prod.demo.catena-x.net')
+     return 'https://centralidp.pre-prod.demo.catena-x.net/auth'
+   if (hostname === 'portal.catena-x.net')
+     return 'https://centralidp.catena-x.net/auth'
+   return 'https://centralidp.dev.demo.catena-x.net/auth'
+ }
+ 
+ export const getClientId = () => 'Cl2-CX-Portal'
+ 
+ //TODO: remove hard coded url and activate after setup of BPDM Api
+ export const getBpdmApiBase = () => {
+   const hostname = getHostname()
+   if (hostname === 'portal.int.demo.catena-x.net')
+     return 'https://partners-pool.int.demo.catena-x.net/api'
+   if (hostname === 'portal-pen.dev.demo.catena-x.net')
+     return 'https://partners-pool-pen.dev.demo.catena-x.net/api'
+   if (hostname === 'portal.beta.demo.catena-x.net')
+     return 'https://partners-pool.beta.demo.catena-x.net/api'
+   if (hostname === 'portal.pre-prod.demo.catena-x.net')
+     return 'https://partners-pool.pre-prod.demo.catena-x.net/api'
+   if (hostname === 'portal.catena-x.net')
+     return 'https://partners-pool.catena-x.net/api'
+   return 'https://partners-pool.dev.demo.catena-x.net/api'
+ }
+ //export const getBpdmApiBase = () =>
+ //  isLocal()
+ //    ? LOCAL_SERVICES_BPDM
+ //    : window.location.origin.replace('portal', 'bpdm')
+ 
+ export const getSemanticApiBase = () => {
+   const hostname = getHostname()
+   if (hostname === 'portal.int.demo.catena-x.net')
+     return 'https://semantics.int.demo.catena-x.net/'
+   if (hostname === 'portal-pen.dev.demo.catena-x.net')
+     return 'https://semantics-pen.dev.demo.catena-x.net/'
+   if (hostname === 'portal.beta.demo.catena-x.net')
+     return 'https://semantics.beta.demo.catena-x.net/'
+   if (hostname === 'portal.pre-prod.demo.catena-x.net')
+     return 'https://semantics.pre-prod.demo.catena-x.net/'
+   if (hostname === 'portal.catena-x.net')
+     return 'https://semantics.catena-x.net/'
+   return 'https://semantics.dev.demo.catena-x.net/'
+ }
+ 
+ export const getClientIdSemantic = () => 'Cl3-CX-Semantic'
+ 
+ export const getClientIdDigitalTwin = () => 'Cl4-CX-DigitalTwin'
+ 
+ const EnvironmentService = {
+   isLocal,
+   getHostname,
+   getApiBase,
+   getAssetBase,
+   getBpdmApiBase,
+   getCentralIdp,
+   getSemanticApiBase,
+   getClientId,
+   getClientIdSemantic,
+   getClientIdDigitalTwin,
+ }
+ 
+ export default EnvironmentService
