d156930612d23d664ec5eda9bbc69433ba89ef6f
Nidhi garg <nidhi.garg@partner.bmw.in>
2022-11-28 17:55:37 +0530

    code pretty

--- cx-portal/src/types/Patterns.ts
+++ cx-portal/src/types/Patterns.ts
@@ -18,54 +18,55 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
- const DOMAIN =
- /([a-z0-9]|[a-z0-9][a-z0-9-]{0,61}[a-z0-9])(\.([a-z0-9]|[a-z0-9][a-z0-9-]{0,61}[a-z0-9])){1,10}/i
+const DOMAIN =
+  /([a-z0-9]|[a-z0-9][a-z0-9-]{0,61}[a-z0-9])(\.([a-z0-9]|[a-z0-9][a-z0-9-]{0,61}[a-z0-9])){1,10}/i
 const URLPATH = /(\/[a-z0-9-._~:/?#[\]@!$&'()*+,;=%]{0,500}){0,20}/
 
 export const Patterns = {
- ID: /^[a-z0-9-_]{1,80}$/i,
- LABEL: /^[a-z0-9-_ ]{1,80}$/i,
- BPN: /^BPNL[0-9A-Z]{12}$/i,
- MAIL: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-z0-9-]+\.)+[a-z]{2,}))$/i,
- DOMAIN: new RegExp(`^${DOMAIN.source}$`, 'i'),
- PATH: new RegExp(`^${URLPATH.source}$`, 'i'),
- URL: new RegExp(
-   `^(https)://(${DOMAIN.source})(:\\d{1,5})?(${URLPATH.source})?$`,
-   'i'
- ),
- NAME: /^([A-Za-zÀ-ÿ-,.']{1,40} ?){1,8}$/i,
- UUID: /^[a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8}$/i,
- COMPANY_NAME: /^[0-9]*?[a-zÀ-ÿ]{1}[0-9]?([a-z0-9À-ÿ-_+=.,:;!?'"&#@()]\s?){1,29}$/i,
- prefix: {
-   BPN: /^BPNL/i,
-   URL: /^https:/i,
-   MAIL: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@/,
-   URNID: /^urn:uuid:[a-z0-9]{8}(-[a-z0-9]{4}){3}-[a-z0-9]{12}$/i,
- },
- SEARCH: /^[a-zA-ZÀ-ÿ0-9 !?@&_\-.]{3,80}$/,
- appMarketCard: {
-   appTitle: /^([A-Za-z.:_@&0-9 -]){5,40}$/,
-   appProvider: /^([A-Za-z ]){3,30}$/,
-   shortDescriptionEN: /^([a-zA-Z0-9 !?@&#'"()_\-=/*.,;:]){10,255}$/,
-   shortDescriptionDE: /^([a-zA-ZÀ-ÿ0-9 !?@&#'"()_\-=/*.,;:]){10,255}$/,
-   useCaseCategory: /^([A-Za-z])$/,
-   appLanguage: /^([A-Za-z ])$/,
-   pricingInformation: /^([A-Za-z0-9/€ ]){1,15}$/,
- },
- appPage: {
-   longDescriptionEN: /^([a-zA-Z0-9 !?@&#'"()[\]_\-+=<>/*.,;:]){10,2000}$/,
-   longDescriptionDE: /^([a-zA-ZÀ-ÿ0-9 !?@&#'"()[\]_\-+=<>/*.,;:]){10,2000}$/,
-   providerHomePage: /^([A-Za-z.:@&0-9 !])+$/,
-   phone: /^\+(\d{2})+(\(\s\d{3}\))?\s?\d{9,20}$/,
- },
- idp: {
-   clientId: /^[a-zA-Z0-9-]*$/,
-   clientSecret: /^[a-zA-Z0-9]*$/,
- },
- connectors: {
-   NAME: /^[a-zA-Z0-9 @]{2,20}$/,
-   COUNTRY: /^[A-Z]{2}$/,
- },
+  ID: /^[a-z0-9-_]{1,80}$/i,
+  LABEL: /^[a-z0-9-_ ]{1,80}$/i,
+  BPN: /^BPNL[0-9A-Z]{12}$/i,
+  MAIL: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-z0-9-]+\.)+[a-z]{2,}))$/i,
+  DOMAIN: new RegExp(`^${DOMAIN.source}$`, 'i'),
+  PATH: new RegExp(`^${URLPATH.source}$`, 'i'),
+  URL: new RegExp(
+    `^(https)://(${DOMAIN.source})(:\\d{1,5})?(${URLPATH.source})?$`,
+    'i'
+  ),
+  NAME: /^([A-Za-zÀ-ÿ-,.']{1,40} ?){1,8}$/i,
+  UUID: /^[a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8}$/i,
+  COMPANY_NAME:
+    /^[0-9]*?[a-zÀ-ÿ]{1}[0-9]?([a-z0-9À-ÿ-_+=.,:;!?'"&#@()]\s?){1,29}$/i,
+  prefix: {
+    BPN: /^BPNL/i,
+    URL: /^https:/i,
+    MAIL: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@/,
+    URNID: /^urn:uuid:[a-z0-9]{8}(-[a-z0-9]{4}){3}-[a-z0-9]{12}$/i,
+  },
+  SEARCH: /^[a-zA-ZÀ-ÿ0-9 !?@&_\-.]{3,80}$/,
+  appMarketCard: {
+    appTitle: /^([A-Za-z.:_@&0-9 -]){5,40}$/,
+    appProvider: /^([A-Za-z ]){3,30}$/,
+    shortDescriptionEN: /^([a-zA-Z0-9 !?@&#'"()_\-=/*.,;:]){10,255}$/,
+    shortDescriptionDE: /^([a-zA-ZÀ-ÿ0-9 !?@&#'"()_\-=/*.,;:]){10,255}$/,
+    useCaseCategory: /^([A-Za-z])$/,
+    appLanguage: /^([A-Za-z ])$/,
+    pricingInformation: /^([A-Za-z0-9/€ ]){1,15}$/,
+  },
+  appPage: {
+    longDescriptionEN: /^([a-zA-Z0-9 !?@&#'"()[\]_\-+=<>/*.,;:]){10,2000}$/,
+    longDescriptionDE: /^([a-zA-ZÀ-ÿ0-9 !?@&#'"()[\]_\-+=<>/*.,;:]){10,2000}$/,
+    providerHomePage: /^([A-Za-z.:@&0-9 !])+$/,
+    phone: /^\+(\d{2})+(\(\s\d{3}\))?\s?\d{9,20}$/,
+  },
+  idp: {
+    clientId: /^[a-zA-Z0-9-]*$/,
+    clientSecret: /^[a-zA-Z0-9]*$/,
+  },
+  connectors: {
+    NAME: /^[a-zA-Z0-9 @]{2,20}$/,
+    COUNTRY: /^[A-Z]{2}$/,
+  },
 }
 
 export const isID = (expr: string) => Patterns.ID.test(expr)
@@ -76,17 +77,17 @@ export const isURL = (expr: string) => Patterns.URL.test(expr)
 export const isUUID = (expr: string) => Patterns.UUID.test(expr)
 export const isCompanyName = (expr: string) => Patterns.COMPANY_NAME.test(expr)
 export const isMailOrEmpty = (expr: string) =>
- expr === '' || Patterns.MAIL.test(expr)
+  expr === '' || Patterns.MAIL.test(expr)
 export const isBPNOrEmpty = (expr: string) =>
- expr === '' || Patterns.BPN.test(expr)
+  expr === '' || Patterns.BPN.test(expr)
 export const isDomainOrEmpty = (expr: string) =>
- expr === '' || Patterns.DOMAIN.test(expr)
+  expr === '' || Patterns.DOMAIN.test(expr)
 export const isURLOrEmpty = (expr: string) =>
- expr === '' || Patterns.URL.test(expr)
+  expr === '' || Patterns.URL.test(expr)
 export const isUUIDOrEmpty = (expr: string) =>
- expr === '' || Patterns.UUID.test(expr)
+  expr === '' || Patterns.UUID.test(expr)
 export const isCName = (expr: string) => Patterns.connectors.NAME.test(expr)
 export const isCountryCode = (expr: string) =>
- Patterns.connectors.COUNTRY.test(expr)
+  Patterns.connectors.COUNTRY.test(expr)
 
 export default Patterns
