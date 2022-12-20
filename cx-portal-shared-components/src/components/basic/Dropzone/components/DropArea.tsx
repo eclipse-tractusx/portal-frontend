7e4dcdd96beaed47a7c61a4acd1a0d5cff1907ab
Bernhard Caspar <bernhard.caspar@raysono.com>
2022-12-20 09:55:19 +0100

    Refactor Dropzone error display

--- cx-portal-shared-components/src/components/basic/Dropzone/components/DropArea.tsx
+++ cx-portal-shared-components/src/components/basic/Dropzone/components/DropArea.tsx
@@ -19,11 +19,12 @@
  ********************************************************************************/
 
 import { Box, Link, useTheme } from '@mui/material'
-import { FunctionComponent, useState } from 'react'
+import { Fragment, FunctionComponent, useState } from 'react'
 import { FileErrorIcon } from '../../CustomIcons/FileErrorIcon'
 import { Typography } from '../../Typography'
 import UploadFileIcon from '@mui/icons-material/UploadFile'
 import { DropZoneDropAreaTranslations } from '../types'
+import { Alert } from '../../Alert'
 
 export interface DropAreaProps {
   translations: DropZoneDropAreaTranslations
@@ -60,102 +61,109 @@ export const DropArea: FunctionComponent<DropAreaProps> = ({
     }
   }
 
-  const hasError = !!error
-  const hasErrorMessage = hasError && error !== true
+  const hasGenericError = error === true
+  const hasErrorMessage = error && typeof error === 'string'
 
-  const errorBackground = hasError && 'declined.main'
+  const errorBackground = hasGenericError && 'declined.main'
   const disabledBackground = disabled && 'action.disabledBackground'
   const draggingBackground = isDragging && 'selected.active'
 
   const borderRadius = 24
-  const borderColor = hasError
+  const borderColor = hasGenericError
     ? encodeURIComponent(theme.palette.danger.danger)
     : 'rgb(7, 73, 133)'
   const dashedBorder = `"data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='${borderRadius}' ry='${borderRadius}' stroke='${borderColor}' stroke-width='2' stroke-dasharray='2%2c 6' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e"`
 
   return (
-    <Box
-      onDragEnter={() => !disabled && setDragging(true)}
-      onDragLeave={() => setDragging(false)}
-      onDrop={() => setDragging(false)}
-      sx={{
-        position: 'relative',
-        transition: theme.transitions.create('background-color'),
-        borderRadius: `${borderRadius}px`,
-        border: 'none',
-        backgroundImage: disabled ? 'none' : `url(${dashedBorder})`,
-        textAlign: 'center',
-        backgroundColor:
-          disabledBackground ||
-          errorBackground ||
-          draggingBackground ||
-          'selected.hover',
-        '&:hover': {
+    <Fragment>
+      <Box
+        onDragEnter={() => !disabled && setDragging(true)}
+        onDragLeave={() => setDragging(false)}
+        onDrop={() => setDragging(false)}
+        sx={{
+          position: 'relative',
+          transition: theme.transitions.create('background-color'),
+          borderRadius: `${borderRadius}px`,
+          border: 'none',
+          backgroundImage: disabled ? 'none' : `url(${dashedBorder})`,
+          textAlign: 'center',
           backgroundColor:
             disabledBackground ||
             errorBackground ||
             draggingBackground ||
-            'selected.focus',
-        },
-      }}
-    >
-      <Box
-        component="label"
-        sx={{
-          padding: size === 'normal' ? 5 : 2.5,
-          display: 'block',
-          cursor: disabled ? 'no-drop' : 'pointer',
+            'selected.hover',
+          '&:hover': {
+            backgroundColor:
+              disabledBackground ||
+              errorBackground ||
+              draggingBackground ||
+              'selected.focus',
+          },
         }}
       >
         <Box
+          component="label"
           sx={{
-            pointerEvents: 'none',
-            display: size === 'normal' ? 'block' : 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            ...(disabled && {
-              '& *': {
-                color: 'action.disabled',
-              },
-            }),
+            padding: size === 'normal' ? 5 : 2.5,
+            display: 'block',
+            cursor: disabled ? 'no-drop' : 'pointer',
           }}
         >
-          <Box sx={{ height: '68px' }}>
-            {hasError ? (
-              <FileErrorIcon
-                size={60}
-                fillColor={theme.palette.danger.danger}
-              />
-            ) : (
-              <UploadFileIcon
-                sx={{ color: 'primary.main', width: '68px', height: '68px' }}
-              />
-            )}{' '}
+          <Box
+            sx={{
+              pointerEvents: 'none',
+              display: size === 'normal' ? 'block' : 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              ...(disabled && {
+                '& *': {
+                  color: 'action.disabled',
+                },
+              }),
+            }}
+          >
+            <Box sx={{ height: '68px' }}>
+              {hasGenericError ? (
+                <FileErrorIcon
+                  size={60}
+                  fillColor={theme.palette.danger.danger}
+                />
+              ) : (
+                <UploadFileIcon
+                  sx={{ color: 'primary.main', width: '68px', height: '68px' }}
+                />
+              )}
+            </Box>
+            <Box>
+              <Typography
+                variant="h3"
+                fontFamily="body1.fontFamily"
+                sx={{
+                  marginTop: size === 'normal' ? 3 : 0,
+                  marginLeft: size === 'normal' ? 0 : 3,
+                  display: 'block',
+                }}
+              >
+                {title}
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{ marginTop: 0.5, display: 'block' }}
+              >
+                {formattedSubtitle}
+              </Typography>
+            </Box>
           </Box>
-          <Box>
-            <Typography
-              variant="h3"
-              fontFamily="body1.fontFamily"
-              sx={{
-                marginTop: size === 'normal' ? 3 : 0,
-                marginLeft: size === 'normal' ? 0 : 3,
-                display: 'block',
-              }}
-            >
-              {hasErrorMessage ? translations.errorTitle : title}
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{ marginTop: 0.5, display: 'block' }}
-            >
-              {hasErrorMessage ? error : formattedSubtitle}
-            </Typography>
+          <Box sx={{ position: 'absolute', visibility: 'hidden' }}>
+            {children}
           </Box>
-        </Box>
-        <Box sx={{ position: 'absolute', visibility: 'hidden' }}>
-          {children}
         </Box>
       </Box>
-    </Box>
+      {hasErrorMessage && (
+        <Box sx={{ marginY: 2 }}>
+          <Alert severity="error">{error}</Alert>
+        </Box>
+      )}
+    </Fragment>
   )
 }
