835d775188161d799547d8bff91cc29d1046b12b
nidhi garg <nidhi.garg@partner.bmw.in>
2023-02-27 13:59:06 +0530

    remove unused code

--- cx-portal-shared-components/src/components/content/Cards/CardHorizontal.tsx
+++ cx-portal-shared-components/src/components/content/Cards/CardHorizontal.tsx
@@ -70,8 +70,6 @@ export const CardHorizontal = ({
           boxShadow: theme.shadows['20'],
         },
       }}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
     >
       <Box
         sx={{
