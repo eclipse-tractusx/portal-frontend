5493fd5987511e27f964eb39bedf65da9504011e
nidhi garg <nidhi.garg@partner.bmw.in>
2023-03-16 17:17:19 +0530

    add policy type to string

--- cx-portal/src/components/pages/AdminBoardDetail/components/BoardPrivacy/index.tsx
+++ cx-portal/src/components/pages/AdminBoardDetail/components/BoardPrivacy/index.tsx
@@ -31,7 +31,7 @@ export default function BoardPrivacy({ item }: { item: AppDetails }) {
     keyPrefix: 'content.appdetail.privacy',
   })
 
-  const renderBoardPrivacy = (policy: PrivacyPolicyType) => {
+  const renderBoardPrivacy = (policy: string) => {
     switch (policy) {
       case PrivacyPolicyType.COMPANY_DATA:
         return <Apartment className="policy-icon" />
@@ -56,7 +56,7 @@ export default function BoardPrivacy({ item }: { item: AppDetails }) {
       </div>
       {item.privacyPolicies && item.privacyPolicies.length ? (
         <div className="policies-list">
-          {item.privacyPolicies.map((policy: PrivacyPolicyType) => (
+          {item.privacyPolicies.map((policy: string) => (
             <Typography
               variant="body2"
               className="policy-name"
