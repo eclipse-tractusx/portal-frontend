beb99927723f93a81abc35ea0cfe68e20060c4ae
manojava-gk <ibm.g_koushik@mercedes-benz.com>
2023-03-29 15:10:12 +0530

    Header text update

--- cx-portal/src/components/pages/ServiceReleaseProcess/components/ServiceDetail.scss
+++ cx-portal/src/components/pages/ServiceReleaseProcess/components/ServiceDetail.scss
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2021, 2023 BMW Group AG
+ * Copyright (c) 2021, 2023 Mercedes-Benz Group AG and BMW Group AG
  * Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
