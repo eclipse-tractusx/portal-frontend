90fba015b53db2521bace28522f4de94fad82cde
nidhi garg <nidhi.garg@partner.bmw.in>
2023-04-04 11:05:36 +0530

    code pretty

--- cx-portal/src/components/overlays/AddUser/AddUserContent.tsx
+++ cx-portal/src/components/overlays/AddUser/AddUserContent.tsx
@@ -126,9 +126,9 @@ export const AddUserContent = ({ idp }: { idp: IdentityProvider }) => {
       const response =
         idp.identityProviderCategoryId !== IDPCategory.KEYCLOAK_SHARED
           ? await addUserIdp({
-            identityProviderId: idp.identityProviderId,
-            user: addUser,
-          }).unwrap()
+              identityProviderId: idp.identityProviderId,
+              user: addUser,
+            }).unwrap()
           : await addTenantUsers([addUser]).unwrap()
       setStatus(AddUserState.SUCCESS)
     } catch (err) {
@@ -182,7 +182,7 @@ export const AddUserContent = ({ idp }: { idp: IdentityProvider }) => {
             loadIndicator="Loading ..."
             loading
             size="medium"
-            onButtonClick={() => { }}
+            onButtonClick={() => {}}
             sx={{ marginLeft: '10px' }}
           />
         ) : (
