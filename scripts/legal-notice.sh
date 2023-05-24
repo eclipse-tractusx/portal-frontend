55f70c0da745b3505c9220b12afe28dbd5d04f12
Evelyn Gurschler <evelyn.gurschler@bmw.de>
2023-05-24 19:23:00 +0200

    feature(legal-notice): add script for value substitution to be used in build workflow

---
+++ scripts/legal-notice.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+###############################################################
+# Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+###############################################################
+
+# Define placeholders from legal-notice.json
+name_anchor='name-placeholder'
+license_anchor='license-placeholder'
+commit_id_anchor='commit-id-placeholder'
+server_url_anchor='server_url-placeholder'
+repository_anchor='repository-placeholder'
+ref_name_anchor='ref-name-placeholder'
+
+# Read values from package.json using jq
+name=$(jq -r '.name' cx-portal/package.json)
+license=$(jq -r '.license' cx-portal/package.json)
+
+# Get commit id
+commit_id=$(git rev-parse HEAD)
+
+# Get GitHub context variables
+server_url="${{ github.server_url }}"
+repository="${{ github.repository }}"
+ref_name="${{ github.ref_name }}"
+
+# Read legal-notice.json as reference
+legal_notice_reference=$(cat cx-portal/src/assets/notice/legal-notice.json)
+
+# Function to check if placeholder substitution was successful
+check_substitution() {
+  if [[ $1 != *"$2"* ]]; then
+    echo "Error: Failed to replace $2"
+    exit 1
+  fi
+}
+
+# Replace placeholders with actual values
+legal_notice_name="${legal_notice_reference/$name_anchor/$name}"
+check_substitution "$legal_notice_name" "$name_anchor"
+echo "Replaced name"
+
+legal_notice_license="${legal_notice_name/$license_anchor/$license}"
+check_substitution "$legal_notice_license" "$license_anchor"
+echo "Replaced license"
+
+legal_notice_commit="${legal_notice_license/$commit_id_anchor/$commit_id}"
+check_substitution "$legal_notice_commit" "$commit_id_anchor"
+echo "Replaced commit ID"
+
+legal_notice_server_url="${legal_notice_commit//$server_url_anchor/$server_url}"
+check_substitution "$legal_notice_server_url" "$server_url_anchor"
+echo "Replaced server URL"
+
+legal_notice_repository="${legal_notice_server_url//$repository_anchor/$repository}"
+check_substitution "$legal_notice_repository" "$repository_anchor"
+echo "Replaced repository"
+
+legal_notice_ref_name="${legal_notice_repository//$ref_name_anchor/$ref_name}"
+check_substitution "$legal_notice_ref_name" "$ref_name_anchor"
+echo "Replaced ref name"
+
+# Write the final result to legal-notice.json
+echo "$legal_notice_ref_name" > cx-portal/src/assets/notice/legal-notice.json
