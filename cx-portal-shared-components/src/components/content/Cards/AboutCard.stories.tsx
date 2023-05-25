842d336bc3b5a3633711cda7d498736c3b4bb397
manojava-gk <ibm.g_koushik@mercedes-benz.com>
2023-05-25 12:29:53 +0530

    updates(about component) : props and dummy data added as per the configuration

--- cx-portal-shared-components/src/components/content/Cards/AboutCard.stories.tsx
+++ cx-portal-shared-components/src/components/content/Cards/AboutCard.stories.tsx
@@ -13,21 +13,11 @@ const Template: ComponentStory<typeof Component> = (args: any) => (
 
 export const AboutCards = Template.bind({})
 AboutCards.args = {
-  title: 'Your product name',
-  subtitles: [
-    'License: Apache-2.0',
-    'Copyright: https://www.eclipse.org/legal/copyright.php',
-    'Licence Path: https://www.eclipse.org/legal',
-    'Commit ID: 12376123618263187',
-  ],
-  links: [
-    {
-      text: 'NOTICE: eclipse-tractusx',
-      url: 'https://github.com/eclipse-tractusx/portal-frontend',
-    },
-    {
-      text: 'Source URL: eclipse-tractusx',
-      url: 'https://github.com/eclipse-tractusx/portal-frontend',
-    },
-  ],
+  name: 'Example product',
+  repositoryPath: 'https://github.com/eclipse-tractusx/example-product',
+  license: 'License: Apache-2.0',
+  licensePath: 'https://github.com/eclipse-tractusx/example-product/blob/v1.0.0-example/LICENSE',
+  noticePath: 'https://github.com/eclipse-tractusx/example-product/blob/v1.0.0-example/NOTICE.md',
+  sourcePath: 'https://github.com/eclipse-tractusx/example-product/tree/v1.0.0-example',
+  commitId: '67e82n0637d585239c72eda374a17b8cb24046ec'
 }
--- cx-portal-shared-components/src/components/content/Cards/AboutCard.tsx
+++ cx-portal-shared-components/src/components/content/Cards/AboutCard.tsx
@@ -2,67 +2,119 @@ import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 import { Box } from '@mui/material'
 import { Typography } from '../../basic/Typography'
 
-function LinkText(props: { title: string; link: string; noBorder: boolean }) {
+function LinkText(props: LinkType) {
   return (
     <Box
       sx={{
         display: 'flex',
         justifyContent: 'space-between',
-        borderBottom: props.noBorder ? '0px' : '1px solid rgba(0, 0, 0, 0.12)',
-        padding: '20px 0px',
+        borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+        padding: '20px 20px',
         cursor: 'pointer',
         placeItems: 'center',
       }}
-      onClick={() => window.open(props.link, '_blank')}
+      onClick={() => window.open(props.url, '_blank')}
     >
-      <Typography>{props.title}</Typography>
+      <Typography>
+        {props.text} {props.url}
+      </Typography>
       <OpenInNewIcon />
     </Box>
   )
 }
 
+function TitleText(props: LinkType) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-start',
+        borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+        padding: '20px 20px',
+        cursor: 'pointer',
+        placeItems: 'center',
+      }}
+      onClick={() => window.open(props.url, '_blank')}
+    >
+      <Typography variant="h3" sx={{ paddingRight: '10px' }}>
+        {props.text}
+      </Typography>
+      <OpenInNewIcon />
+    </Box>
+  )
+}
+
+type LinkType = {
+  text: string
+  url: string
+}
+
 export const AboutCard = (props: {
-  title: string
-  subtitles: string[]
-  links: { text: string; url: string }[]
-  footerText: string
+  name: string
+  repositoryPath: string
+  license: string
+  licensePath: string
+  noticePath: string
+  sourcePath: string
+  commitId: string
 }) => {
   return (
     <Box
       sx={{
-        background: '#FFFFFF',
-        boxShadow: '0px 5px 10px rgba(80, 80, 80, 0.3)',
-        borderRadius: '8px',
-        padding: '20px 20px 0px 20px',
+        display: 'flex',
+        justifyContent: 'center',
+        maxWidth: '1200px',
+        alignItems: 'center',
       }}
     >
-      <Box>
-        <Typography variant="h3">{props.title}</Typography>
+      <Box
+        sx={{
+          background: '#FFFFFF',
+          boxShadow: '0px 5px 10px rgba(80, 80, 80, 0.3)',
+          borderRadius: '8px',
+          width: '100%',
+        }}
+      >
         <Box>
+          <TitleText text={props.name} url={props.repositoryPath} />
           <Box
             sx={{
               borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
             }}
           >
-            {props.subtitles.map((str) => (
+            {props.license && (
               <Typography
                 sx={{
-                  padding: '10px 0px',
+                  padding: '20px 20px',
                 }}
-                key={str}
               >
-                {str}
+                {props.license}
               </Typography>
-            ))}
+            )}
           </Box>
-          {props.links.map((link, index) => (
+          {props.licensePath && (
             <LinkText
-              key={link.text}
-              title={link.text}
-              link={link.url}
-              noBorder={props.links.length - 1 === index}
+              text="Licence Path: "
+              url={props.licensePath}
             />
-          ))}
+          )}
+          {props.noticePath && (
+            <LinkText text="NOTICE: " url={props.noticePath} />
+          )}
+          {props.sourcePath && (
+            <LinkText text="Source URL: " url={props.sourcePath} />
+          )}
+          {props.commitId && (
+            <Box>
+              <Typography
+                sx={{
+                  padding: '20px 20px',
+                }}
+              >
+                Commit Id : {props.commitId}
+              </Typography>
+            </Box>
+          )}
         </Box>
       </Box>
     </Box>
