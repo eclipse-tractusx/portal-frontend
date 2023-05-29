8760253122b939a3337a6d3d17ac557dbcd226c1
manojava-gk <ibm.g_koushik@mercedes-benz.com>
2023-05-29 14:30:14 +0530

    bugfix(static template) : code smell issue fix

--- cx-portal/src/components/shared/templates/StaticTemplateResponsive/Cards/TextCenterAlignedBody2.tsx
+++ cx-portal/src/components/shared/templates/StaticTemplateResponsive/Cards/TextCenterAlignedBody2.tsx
@@ -60,7 +60,7 @@ export default function TextCenterAlignedBody2({
               i18nKey={subtitle}
               components={[
                 <span key={subtitle} className="tooltiptext"></span>,
-                <br />,
+                <br key={subtitle} />,
               ]}
             ></Trans>
           ))}
