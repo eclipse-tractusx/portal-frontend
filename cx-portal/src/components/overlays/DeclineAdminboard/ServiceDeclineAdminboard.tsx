ab8d20f15d799c3d91aeb301647a87b3d37e1cbb
nidhi garg <nidhi.garg@partner.bmw.in>
2023-06-08 13:26:04 +0530

    fix(sonar bugs): fixing void code smells

--- cx-portal/src/components/overlays/DeclineAdminboard/ServiceDeclineAdminboard.tsx
+++ cx-portal/src/components/overlays/DeclineAdminboard/ServiceDeclineAdminboard.tsx
@@ -59,7 +59,7 @@ export default function ServiceDeclineAdminboard({ id }: { id: string }) {
   return (
     <>
       <DeclineAdminBoard
-        handleConfirm={() => void (async () => await onConfirm)}
+        handleConfirm={() => void (() => onConfirm)}
         confirmBtn={t('adminBoard.declineModal.confirm')}
         closeBtn={t('adminBoard.declineModal.close')}
         title={t('adminBoard.declineModal.title').replace(
