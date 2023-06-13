9c663acef492fadcd3e45ace47525f97cba6f049
nidhi garg <nidhi.garg@partner.bmw.in>
2023-06-13 18:51:11 +0530

    fix(sonar bugs): fixing sonar bugs

--- cx-portal/src/services/CommonService.ts
+++ cx-portal/src/services/CommonService.ts
@@ -22,11 +22,6 @@ import { getApiBase, getAssetBase } from './EnvironmentService'
 import i18next from 'i18next'
 import UserService from './UserService'
 
-export interface LeadPictureResponse {
-  url: string | ArrayBuffer | null
-  text: ''
-}
-
 const getName = (app: any) => app.name ?? ''
 const getDescription = (app: any) =>
   app.shortDescription === 'ERROR' ? '' : app.shortDescription
@@ -39,7 +34,7 @@ const getPrice = (app: any) => (app.price === 'ERROR' ? '' : app.price)
 const fetchLeadPictureImage = (data: any[]) => {
   const promises = data?.map((app: any) => {
     return [
-      new Promise<void>((resolve, reject) => {
+      new Promise((resolve, reject) => {
         let url = `${getApiBase()}/api/apps/${
           app.id
         }/appDocuments/${isValidPictureId(app.leadPictureId)}`
@@ -86,7 +81,7 @@ const fetchLeadPictureImage = (data: any[]) => {
 const fetchLeadPictures = (images: string[], appId: string) => {
   const promises = images?.map((image: any) => {
     return [
-      new Promise<LeadPictureResponse>((resolve, reject) => {
+      new Promise((resolve, reject) => {
         let url = ''
         if (!image.documentId) {
           url = `${getApiBase()}/api/apps/${appId}/appDocuments/${isValidPictureId(
