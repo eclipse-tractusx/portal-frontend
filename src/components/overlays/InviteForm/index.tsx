78624f598d510850a804e7484c0e2f23b4aca105
manojava-gk <ibm.g_koushik@mercedes-benz.com>
2023-09-12 17:47:44 +0530

    code formatting

--- src/components/overlays/InviteForm/index.tsx
+++ src/components/overlays/InviteForm/index.tsx
@@ -130,7 +130,11 @@ export const InviteForm = ({
           <Button variant="outlined" onClick={(e) => handleOverlayClose(e)}>
             {`${t('global.actions.cancel')}`}
           </Button>
-          <Button name="send" disabled={inpValid[4] || state === 'busy'} onClick={doSubmit}>
+          <Button
+            name="send"
+            disabled={inpValid[4] || state === 'busy'}
+            onClick={doSubmit}
+          >
             {`${t('content.invite.invite')}`}
           </Button>
         </DialogActions>
