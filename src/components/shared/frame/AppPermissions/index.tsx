d34d775580cabde48f90a6754bfda7f82bd057d7
nidhigarg-bmw <101316912+nidhigarg-bmw@users.noreply.github.com>
2023-11-13 10:25:24 +0530

    fix(sonar bugs): fixed sonar bugs errors (#350)

--- src/components/shared/frame/AppPermissions/index.tsx
+++ src/components/shared/frame/AppPermissions/index.tsx
@@ -40,6 +40,9 @@ export const AppPermissions = ({ user }: { user: TenantUserDetails }) => {
         onClick={() => {
           navigate(`/appdetail/${id}`)
         }}
+        onKeyUp={() => {
+          // do nothing
+        }}
       >
         {data ? data.title : id}
         <span style={{ marginLeft: '20px', color: '#cccccc' }}>
