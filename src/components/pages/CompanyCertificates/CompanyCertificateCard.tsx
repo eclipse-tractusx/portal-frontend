66864e11e79da5332b06fd9573ac1d48ecc0c518
manojava-gk <ibm.g_koushik@mercedes-benz.com>
2024-04-02 19:30:27 +0530

    fix(company certificate): - implementation of new ui design for company certificates. show error icon on certificate deletion error scenario. update api endpoint for fetching certificate document. use listing page api to fetch certificates in details page

--- src/components/pages/CompanyCertificates/CompanyCertificateCard.tsx
+++ src/components/pages/CompanyCertificates/CompanyCertificateCard.tsx
@@ -18,10 +18,14 @@
  ********************************************************************************/
 
 import { useTranslation } from 'react-i18next'
-import { Logo, Typography } from '@catena-x/portal-shared-components'
+import {
+  IconButton,
+  Tooltips,
+  Typography,
+} from '@catena-x/portal-shared-components'
 import './CompanyCertificate.scss'
 import { type ComapnyCertificateData } from 'features/companyCertification/companyCertificateApiSlice'
-import { Box } from '@mui/material'
+import { Box, ClickAwayListener } from '@mui/material'
 import { useState } from 'react'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import { useDispatch } from 'react-redux'
@@ -29,6 +33,17 @@ import { OVERLAYS, ROLES } from 'types/Constants'
 import { show } from 'features/control/overlay'
 import dayjs from 'dayjs'
 import UserService from 'services/UserService'
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined'
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded'
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded'
+import WarningRoundedIcon from '@mui/icons-material/WarningRounded'
+
+enum CompanyCertificateStatus {
+  INACTIVE = 'INACTIVE',
+  ACTIVE = 'ACTIVE',
+  PENDING = 'PENDING',
+}
 
 export default function CompanyCertificateCard({
   item,
@@ -49,40 +64,267 @@ export default function CompanyCertificateCard({
         item.companyCertificateType
       )
     )
-  return (
-    <Box className="card-container">
-      <Box className="first-container">
-        <Logo />
-      </Box>
-      <Box className="second-container">
-        <Box className="top-container">
-          <Typography variant="h4">{item.companyCertificateType}</Typography>
-          <Box
-            className="dots"
-            onClick={() => {
-              setDotsMenu(!dotsMenu)
+
+  const today = dayjs()
+  const isWarning = dayjs(item.validTill).diff(today, 'days') < 30
+
+  const PendingIcon = () => {
+    return (
+      <Tooltips
+        additionalStyles={{
+          cursor: 'pointer',
+          marginTop: '30px !important',
+        }}
+        tooltipPlacement="bottom-start"
+        tooltipText={t('content.companyCertificate.tooltips.pending')}
+        children={
+          <IconButton
+            size="small"
+            sx={{
+              width: '48px',
+              height: '36px',
+              backgroundColor: '#FFECBD',
+              borderRadius: '74px',
+              color: '#FFECBD',
+              ':hover': {
+                backgroundColor: '#FFECBD',
+                borderColor: '#FFECBD',
+                color: '#FFECBD',
+              },
+              ':active': {
+                boxShadow: 'none',
+              },
+              ':focus': {
+                boxShadow: 'none',
+              },
             }}
           >
-            <MoreVertIcon />
-            {dotsMenu && (
-              <Box className="more-container">
-                <Typography variant="label3" onClick={handleView}>
-                  {t('content.companyCertificate.view')}{' '}
-                </Typography>
-                {UserService.hasRole(ROLES.SUBSCRIBE_SERVICE_MARKETPLACE) && (
+            <AccessTimeOutlinedIcon
+              style={{
+                color: '#975B27',
+                height: '20px',
+              }}
+            />
+          </IconButton>
+        }
+      />
+    )
+  }
+
+  const InactiveIcon = () => {
+    return (
+      <Tooltips
+        additionalStyles={{
+          cursor: 'pointer',
+          marginTop: '30px !important',
+        }}
+        tooltipPlacement="bottom-start"
+        tooltipText={t('content.companyCertificate.tooltips.inactive')}
+        children={
+          <IconButton
+            size="small"
+            sx={{
+              width: '48px',
+              height: '36px',
+              marginLeft: '10px',
+              backgroundColor: '#FEE7E2',
+              borderRadius: '74px',
+              color: '#FEE7E2',
+              ':hover': {
+                backgroundColor: '#FEE7E2',
+                borderColor: '#FEE7E2',
+                color: '#FEE7E2',
+              },
+              ':active': {
+                boxShadow: 'none',
+              },
+              ':focus': {
+                boxShadow: 'none',
+              },
+            }}
+          >
+            <CancelRoundedIcon
+              style={{
+                color: '#FF532F',
+                height: '20px',
+              }}
+            />
+          </IconButton>
+        }
+      />
+    )
+  }
+
+  const ActiveIcon = () => {
+    return (
+      <Tooltips
+        additionalStyles={{
+          cursor: 'pointer',
+          marginTop: '30px !important',
+        }}
+        tooltipPlacement="bottom-start"
+        tooltipText={t('content.companyCertificate.tooltips.active')}
+        children={
+          <IconButton
+            size="small"
+            sx={{
+              width: '48px',
+              height: '36px',
+              marginLeft: '10px',
+              backgroundColor: '#E2F6C7',
+              borderRadius: '74px',
+              color: '#E2F6C7',
+              ':hover': {
+                backgroundColor: '#E2F6C7',
+                borderColor: '#E2F6C7',
+                color: '#E2F6C7',
+              },
+              ':active': {
+                boxShadow: 'none',
+              },
+              ':focus': {
+                boxShadow: 'none',
+              },
+            }}
+          >
+            <CheckCircleRoundedIcon
+              style={{
+                color: '#5C8D45',
+                height: '20px',
+              }}
+            />
+          </IconButton>
+        }
+      />
+    )
+  }
+
+  const DeleteIcon = () => {
+    return (
+      <IconButton
+        color="secondary"
+        size="small"
+        sx={{
+          marginLeft: '10px',
+          width: '48px',
+          height: '36px',
+          borderRadius: '74px',
+        }}
+        onClick={handleDelete}
+        disabled={!UserService.hasRole(ROLES.SUBSCRIBE_SERVICE_MARKETPLACE)}
+      >
+        <DeleteOutlinedIcon
+          style={{
+            color: 'secondary',
+            height: '20px',
+          }}
+        />
+      </IconButton>
+    )
+  }
+
+  const DotsMenu = () => {
+    return (
+      <ClickAwayListener
+        onClickAway={() => {
+          if (dotsMenu) setDotsMenu(!dotsMenu)
+        }}
+      >
+        <Box
+          className="dots"
+          onClick={() => {
+            setDotsMenu(!dotsMenu)
+          }}
+        >
+          <MoreVertIcon />
+          {dotsMenu && (
+            <Box className="more-container">
+              <Typography variant="label3" onClick={handleView}>
+                {t('content.companyCertificate.view')}{' '}
+              </Typography>
+              {UserService.hasRole(ROLES.SUBSCRIBE_SERVICE_MARKETPLACE) &&
+                item.companyCertificateStatus ===
+                  CompanyCertificateStatus.ACTIVE && (
                   <Typography variant="label3" onClick={handleDelete}>
                     {t('content.companyCertificate.delete')}{' '}
                   </Typography>
                 )}
-              </Box>
-            )}
-          </Box>
+            </Box>
+          )}
+        </Box>
+      </ClickAwayListener>
+    )
+  }
+
+  const DateAndWarningIcon = () => {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="label3">
+          {t('content.companyCertificate.validtill')} :{' '}
+          {dayjs(item.validTill).format('YYYY-MM-DD')}
+        </Typography>
+        {isWarning && (
+          <Tooltips
+            additionalStyles={{
+              cursor: 'pointer',
+              marginTop: '30px !important',
+            }}
+            tooltipPlacement="bottom-start"
+            tooltipText={t('content.companyCertificate.tooltips.warning')}
+            children={
+              <WarningRoundedIcon
+                style={{
+                  color: '#FF4444',
+                  height: '20px',
+                  marginLeft: '10px',
+                }}
+              />
+            }
+          />
+        )}
+      </Box>
+    )
+  }
+  return (
+    <Box className="card-container">
+      <Box
+        className="first-container"
+        sx={{
+          backgroundImage: 'url(/certificate-sample.png)',
+          backgroundColor: '#F2F3FB',
+        }}
+      ></Box>
+      <Box className="second-container">
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'end',
+            padding: '0px 20px 20px',
+          }}
+        >
+          {item.companyCertificateStatus ===
+            CompanyCertificateStatus.PENDING && <PendingIcon />}
+          {item.companyCertificateStatus ===
+            CompanyCertificateStatus.INACTIVE && <InactiveIcon />}
+          {item.companyCertificateStatus ===
+            CompanyCertificateStatus.ACTIVE && (
+            <>
+              <ActiveIcon />
+              <DeleteIcon />
+            </>
+          )}
+        </Box>
+        <Box className="top-container">
+          <Typography variant="h4">{item.companyCertificateType}</Typography>
+          <DotsMenu />
         </Box>
         <Box className="bottom-container">
-          <Typography variant="label3">
-            {t('content.companyCertificate.validtill')} :{' '}
-            {dayjs(item.validTill).format('YYYY-MM-DD')}
-          </Typography>
+          <DateAndWarningIcon />
           <Box className="link" onClick={handleView}>
             <Typography variant="label3">
               {t('content.companyCertificate.morelink')}
