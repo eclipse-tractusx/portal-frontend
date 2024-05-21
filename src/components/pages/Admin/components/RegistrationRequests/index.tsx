622e4e7677f930902f2dbae58b26ffd3eeea639a
nidhigarg-bmw <101316912+nidhigarg-bmw@users.noreply.github.com>
2024-05-21 18:41:27 +0530

    fix(application request): add open overlay on progress button click (#808)
    https://github.com/eclipse-tractusx/portal-frontend/issues/740

--- src/components/pages/Admin/components/RegistrationRequests/index.tsx
+++ src/components/pages/Admin/components/RegistrationRequests/index.tsx
@@ -42,6 +42,11 @@ import CheckListStatusOverlay from './components/CheckList/CheckListStatusOverla
 import ConfirmCancelOverlay from './ConfirmationOverlay/ConfirmCancelOverlay'
 import type { AppDispatch } from 'features/store'
 
+enum TableField {
+  DETAIL = 'detail',
+  STATUS = 'status',
+}
+
 export default function RegistrationRequests() {
   const { t } = useTranslation()
 
@@ -76,13 +81,18 @@ export default function RegistrationRequests() {
   const [confirmCancelModalOpen, setConfirmCancelModalOpen] =
     useState<boolean>(false)
   const [selectedRequestName, setSelectedRequestName] = useState<string>('')
+  const [selectedActiveTab, setSelectedActiveTab] = useState<number>(0)
   const onTableCellClick = (params: GridCellParams) => {
     // Show overlay only when detail field clicked
-    if (params.field === 'detail') {
+    if (
+      params.field === TableField.DETAIL ||
+      params.field === TableField.STATUS
+    ) {
       setSelectedRequestId(params.row.applicationId)
       setSelectedRequest(params.row)
       dispatch(fetchCompanyDetail(params.row.applicationId))
       setOverlayOpen(true)
+      setSelectedActiveTab(params.field === TableField.DETAIL ? 0 : 1)
     }
   }
 
@@ -158,6 +168,7 @@ export default function RegistrationRequests() {
             openDialog: overlayOpen,
             selectedRequest,
             selectedRequestId,
+            selectedActiveTab,
             handleOverlayClose: () => {
               setOverlayOpen(false)
             },
