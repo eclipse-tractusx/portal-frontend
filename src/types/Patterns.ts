351015fea41848aa88c451c7ed4cf758c06dbe88
Jordan Gerada <138688996+JordanGerada3@users.noreply.github.com>
2024-08-15 15:30:02 +0200

    feat: CHERRY-PICK REGEX to allow long company names in BP invite (#82)
    Co-authored-by: Karsten Thiems <150006841+typecastcloud@users.noreply.github.com>

--- src/types/Patterns.ts
+++ src/types/Patterns.ts
@@ -49,8 +49,7 @@ export const Patterns = {
   URL: urlPattern,
   UUID: /^[a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8}$/i,
   EXTID: /^[a-z0-9]{6,36}$/i,
-  COMPANY_NAME:
-    /^\d*?[a-zÀ-ÿ]\d?([a-z0-9À-ÿ!?@&_£$€¥\-.,:;'()*+#%=]\s?){2,40}$/i,
+  COMPANY_NAME: /^(?!.*\s$)([\wÀ-ÿ£$€¥¢@%*+\-/\\,.:;=<>!?&^#'\x22()[\]]\s?){1,160}$/,
   personName: personNamePattern,
   name: /^([A-Za-z\u00C0-\u017F-,.'](?!.*[-,.]{2})[A-Za-z\u00C0-\u017F-,.']{0,40} ?)[^ –]{1,40}$/,
   zipcode: /^[A-Z0-9-]{1,8}$/,
