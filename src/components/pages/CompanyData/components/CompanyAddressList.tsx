59a73c837f65c3ecd267df1567d50bce708f0fcd
manojava-gk <ibm.g_koushik@mercedes-benz.com>
2024-09-06 11:59:42 +0530

    fix(chor): pretty

--- src/components/pages/CompanyData/components/CompanyAddressList.tsx
+++ src/components/pages/CompanyData/components/CompanyAddressList.tsx
@@ -193,7 +193,9 @@ export const CompanyAddressList = ({
           buttons={[
             {
               title: t('content.companyData.table.buttonSite'),
-              click: () => { handleSecondButtonClick() },
+              click: () => {
+                handleSecondButtonClick()
+              },
               icon: <AddCircleOutlineIcon />,
             },
             {
