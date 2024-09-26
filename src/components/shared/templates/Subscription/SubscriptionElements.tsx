e6026b6b3a85fb2c395450f3ec069fb0b47ee605
lavanya-bmw <lavanya.a@partner.bmw.in>
2024-09-26 12:07:16 +0530

    feat(service subscription): rename 'Configure' button to 'Activate'

--- src/components/shared/templates/Subscription/SubscriptionElements.tsx
+++ src/components/shared/templates/Subscription/SubscriptionElements.tsx
@@ -187,7 +187,7 @@ export default function SubscriptionElements({
           <>
             <Chip
               color="primary"
-              label={t('content.appSubscription.configureBtn')}
+              label={t('content.appSubscription.activateBtn')}
               type="plain"
               variant="filled"
               onClick={() => {
