9ab546aced79bc0e83a371f22a89e4532db03e98
Jordan Gerada <138688996+JordanGerada3@users.noreply.github.com>
2024-09-26 09:15:21 +0200

    fix: disable menu item, & pages (#171)

--- src/types/cfx/Config.tsx
+++ src/types/cfx/Config.tsx
@@ -80,7 +80,7 @@ import CompanySubscriptions from 'components/pages/CompanySubscriptions'
 import CompanySubscriptionDetail from 'components/pages/CompanySubscriptions/CompanySubscriptionDetail'
 import { MENUS, PAGES } from './Constants'
 import AdminCredential from 'components/pages/AdminCredential'
-import OnboardingServiceProvider from 'components/pages/OnboardingServiceProvider/OnboardingServiceProvider'
+//import OnboardingServiceProvider from 'components/pages/OnboardingServiceProvider/OnboardingServiceProvider'
 
 /**
  * ALL_PAGES
@@ -515,10 +515,10 @@ export const ALL_PAGES: IPage[] = [
       </Route>
     ),
   },
-  {
+  /*{
     name: PAGES.MANAGEMENT_ONBOARDING_SERVICE_PROVIDER,
     element: <OnboardingServiceProvider />,
-  },
+  },*/
 ]
 
 /**
@@ -583,13 +583,13 @@ export const mainMenuFullTree = [
       { name: PAGES.SERVICE_ADMIN_BOARD }, // Admin Board -> Service Marketplace
     ],
   },
-  {
+  /* {
     name: MENUS.ON_BOARDING_MANAGEMENT,
     children: [
       { name: PAGES.MANAGEMENT_ONBOARDING_SERVICE_PROVIDER },
       { name: PAGES.TECH_USER_MANAGEMENT },
     ],
-  },
+  },*/
 ]
 
 /**
