79b36dd5d042c3605fe20678613da2ffd3367a8a
m.usman <muhammad.usman@cofinity-x.com>
2024-10-04 17:24:43 +0200

    fix: service marketplace and detail images

--- src/components/pages/ServiceMarketplace/RecommendedServices.tsx
+++ src/components/pages/ServiceMarketplace/RecommendedServices.tsx
@@ -27,7 +27,7 @@ import { useNavigate } from 'react-router-dom'
 import type { ServiceRequest } from 'features/serviceMarketplace/serviceApiSlice'
 import './ServiceMarketplace.scss'
 import NoItems from '../NoItems'
-import { getAssetBase } from 'services/EnvironmentService'
+import { getApiBase, getAssetBase } from 'services/EnvironmentService'
 import { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ServiceTypeIdsEnum } from 'features/serviceManagement/apiSlice'
@@ -73,7 +73,11 @@ export default function RecommendedServices({
               <CardHorizontal
                 borderRadius={6}
                 imageAlt="App Card"
-                imagePath={`${getAssetBase()}/images/content/ServiceMarketplace.png`}
+                imagePath={
+                  service?.leadPictureId
+                    ? `${getApiBase()}/api/administration/documents/${service.leadPictureId}`
+                    : `${getAssetBase()}/images/content/ServiceMarketplace.png`
+                }
                 label={service.provider}
                 buttonText="Details"
                 onBtnClick={() => {
