e46ce866b08ac558c6289dd5d02bbcdea7deed37
m.usman <muhammad.usman@cofinity-x.com>
2024-10-11 10:20:12 +0200

    chore: move service enum to api type

--- src/types/Constants.ts
+++ src/types/Constants.ts
@@ -240,8 +240,3 @@ export enum HINTS {
   COMING_SOON = 'coming_soon',
   NEW = 'new',
 }
-
-export enum SORTING_TYPE {
-  RELEASE_DATE_DESC = 'ReleaseDateDesc',
-  PROVIDER_DESC = 'ProviderDesc',
-}
