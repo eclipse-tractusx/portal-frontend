35493755627b94a1604abf6cf8d7360ae302e627
Jordan Gerada <138688996+JordanGerada3@users.noreply.github.com>
2024-11-20 19:10:12 +0100

    fix: update osp customer overview table cell definition to handle empty array (#354)

--- src/components/pages/OnboardingServiceProvider/OnboardingServiceProvider.tsx
+++ src/components/pages/OnboardingServiceProvider/OnboardingServiceProvider.tsx
@@ -390,7 +390,7 @@ const OnboardingServiceProvider = () => {
                   flex: 1,
                   sortable: false,
                   renderCell: ({ row }: { row: networkCompany }) =>
-                    row?.identityProvider?.[0]?.alias,
+                    row?.identityProvider?.[0]?.alias ?? 'No IDP available',
                 },
                 {
                   field: 'activeUsers',
