5347e03b3793440ba68cc031f35be06a2b2b7df1
Sana Saleem <sana.saleem@cofinity-x.com>
2025-04-16 15:52:45 +0200

    Updated requested changes

--- cypress/e2e/tests/technicalUserManagement.cy.ts
+++ cypress/e2e/tests/technicalUserManagement.cy.ts
@@ -1,9 +1,28 @@
+/********************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
 import {
   getRandomItem,
   generateDynamicUsername,
   generateDynamicDescription,
 } from '../../support/utils/dataGenerator/technicalUserManagement'
-import { TechnicalUserManagementPage } from '../../support/pages/TechnicalUserManagementPage'
+import { TechnicalUserManagementPage } from '../../support/pages/technicalUserManagementPage'
 const technicalPage = new TechnicalUserManagementPage()
 
 describe('Technical User Creation', () => {
@@ -41,6 +60,8 @@ describe('Technical User Creation', () => {
           // Verify the success message and close the dialog
           technicalPage.verifySuccessMessage()
           technicalPage.clickClose()
+
+          // This code will never execute until the HTML lang bug is fixed and the value is dynamically updated to "de".
         } else {
           technicalPage.DE_CreateTechnicalUser(
             username,
